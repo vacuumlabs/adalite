d4d0e44e7a118da4a38024ba792787829081799f
refi93 <rafael.korbas@gmail.com>
2018-01-23 15:24:40 +0100

    recreated the hashing function in cardano-sl

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # cardano
 
 A very simple Cardano wallet made in Node.js
+
+To run just type npm-install and then node app.js
--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var sha1 = require('sha1');
 var app = express();
+var cbor = require('cbor');
+var blake2 = require('blake2');
+
+// this is the hash function used in cardano-sl
+function hash(input) {
+  var h = blake2.createHash('blake2b', {digestLength: 32});
+  h.update(cbor.encode(input));
+  return h.digest("hex")
+}
 
 app.get('/', function (req, res) {
-  res.send(sha1('<b>Hello World!</b>'));
+  //res.send(sha1('<b>Hello World!</b>'));
+  res.send(hash("AAA"));
 });
 
 app.listen(3000, function () {
