5020d32ed3b6d96e9a20cf4d2b48cc65954c2b06
refi93 <rafael.korbas@gmail.com>
2018-01-29 16:08:38 +0100

    succesfully encoded a hardcoded transaction

--- app.js
+++ app.js
@@ -3,19 +3,183 @@ var sha1 = require('sha1');
 var app = express();
 var cbor = require('cbor');
 var blake2 = require('blake2');
+var request = require('request');
+var base58 = require('bs58');
 
 // this is the hash function used in cardano-sl
 function hash(input) {
-  var h = blake2.createHash('blake2b', {digestLength: 32});
-  h.update(cbor.encode(input));
-  return h.digest("hex")
+    var h = blake2.createHash('blake2b', {digestLength: 32});
+    h.update(cbor.encode(input));
+    return h.digest("hex")
+}
+
+function getAddressStatus(address, callback) {
+    var url = 'https://cardanoexplorer.com/api/addresses/summary/' + address;
+    
+    var result = undefined;
+
+    return request({
+      'url' : url,
+      'method' : 'GET'
+    }, function (err, output, body) {
+        callback(JSON.parse(body));
+    });
+}
+
+function hex2buf(hexString) {
+    return Buffer.from(hexString, 'hex');
+}
+
+class TxInput {
+    constructor (txId) {
+        this.id = txId;
+    }
+    
+    encodeCBOR (encoder) {
+        const tagged = new cbor.Tagged(24, cbor.encode([
+            hex2buf(this.id),
+            0
+        ]));
+
+        return encoder.pushAny(tagged);
+    }
+}
+
+class TxOutput {
+    constructor (walletAddress, coins) {
+        this.walletAddress = walletAddress;
+        this.coins = coins;
+    }
+
+    encodeCBOR (encoder) {
+        return encoder.pushAny([this.walletAddress, this.coins]);
+    }
+}
+
+class WalletAddress {
+    constructor (address) {
+        this.address = address;
+    }
+
+    encodeCBOR (encoder) {
+        return encoder.push(base58.decode(this.address));
+    }
+}
+
+class Witness {
+    constructor (publicKey, signature) {
+        this.publicKey = publicKey;
+        this.signature = signature;
+    }
+
+    encodeCBOR (encoder) {
+        return encoder.pushAny(new cbor.Tagged(24, cbor.encode([hex2buf(this.publicKey), hex2buf(this.signature)])));
+    }
+}
+
+class CBORIndefiniteLengthArray {
+    constructor (elements) {
+        this.elements = elements;
+    }
+
+    encodeCBOR (encoder) {
+
+        var elementsEncoded = cbor.encode(this.elements);
+        elementsEncoded[0] = 0x9f;
+
+        return encoder.push(Buffer.concat([elementsEncoded, Buffer.from('ff', 'hex')]));
+    }
+}
+
+class CBORIndexedMap {
+    constructor (elements) {
+        this.elements = elements;
+    }
+
+    encodeCBOR (encoder) {
+        // example of the transformation: [2,6,7] => [[0,2], [1, 6], [2,7]]
+        return encoder.pushAny(this.elements.map((elem, i) => { return [i, elem]; }));
+    }
+}
+
+class Transaction {
+    constructor (inputs, outputs, witnesses) {
+        this.inputs = inputs;
+        this.outputs = outputs;
+        this.witnesses = witnesses;
+    }
+
+    encodeCBOR (encoder) {
+        return encoder.pushAny([
+            [
+                new CBORIndefiniteLengthArray(new CBORIndexedMap(this.inputs)),
+                new CBORIndefiniteLengthArray(this.outputs),
+                {}
+            ],
+            new CBORIndexedMap(this.witnesses)
+        ]);
+    }
+}
+
+function getTransaction() {
+    return new Transaction(
+        [
+            new TxInput('E88716E0E5060A92DC3B6441C4F3BE45B3575A99799A737F3B07732656B03D18')
+        ]
+        ,
+        [
+            new TxOutput(
+                new WalletAddress('DdzFFzCqrhtCAAhzES3fi9ow81cyWKxJ4dFm4fUVHgaqToJtnmVkgRDFhGQE2S5U5G6zuHXYna5jaDE1bpvh4jymoGpVzzseNH8KAeUX'),
+                5651560
+            ),
+            new TxOutput(
+                new WalletAddress('DdzFFzCqrht3ZMo1JFBiHJJxQAudUNt4eLfRhV7uBHxX1E3XoDWSru89MSenwacBdLjRTQL68WWsWsRjfJRSf5A89SpFDShuZUkJxdfc'),
+                1
+            )
+        ],
+        [ // witnessovia inputov
+            new Witness(
+                // public key
+                '76EAEF312A3E90C13B0EA1A7B3C0E16D8549345BEC84826D9B1CE98C57AD8BADBA8598CF9314D97DF4BA0051E1471A99C4F77A3021C77212BBCA6AEA699A38DB',                    // signature                    
+                // signature
+                'ACCD5281BA0E7ADA54A6456D1DF8DCD05BF9A96BDD7A5C2C41DE10FFBF66E41CD729F81396F166A2E0E64422637BFF3C914CB62BE0E2694975ADE111EC75F405'
+            )
+        ]
+    );
 }
 
 app.get('/', function (req, res) {
-  //res.send(sha1('<b>Hello World!</b>'));
-  res.send(hash("AAA"));
+    //res.send(sha1('<b>Hello World!</b>'));
+
+    // takto sa zahashuje prazdny objekt Atributes ()
+    //res.send(hash({}));
+
+    // hash pre CCoin 1 objekt - zahashuje priamo hodnotu
+    // res.send(hash(1));
+
+    /* hash pre objekt User 
+    data User
+    = Login { login :: String
+            , age   :: Int }
+    | FullName { firstName :: String
+               , lastName  :: String
+               , sex       :: Bool }
+    deriving (Show, Eq)
+
+    konkretne instancia FullName "AAA" "BBB" True
+
+    podobne sa hashuju aj ine objekty. Ta 1 na zaciatku je "tag" indikujuci,
+    ze sa pouzije druhy datovy typ, t.j. FullName
+    */
+    //res.send(hash([1, "AAA", "BBB", true]))
+  
+    res.send(cbor.encode(getTransaction()).toString('hex'));
+
+    //var address = 'DdzFFzCqrht3ZMo1JFBiHJJxQAudUNt4eLfRhV7uBHxX1E3XoDWSru89MSenwacBdLjRTQL68WWsWsRjfJRSf5A89SpFDShuZUkJxdfc';
+  
+    //getAddressStatus(address, result => res.json(result))
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
