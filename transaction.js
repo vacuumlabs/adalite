94fdea5a38e2884c77d4152d0c5fad08be462a47
jan_hreha <uni.jano@gmail.com>
2018-02-06 13:53:22 +0100

    Refactor: cleanupcode into modules

---
+++ transaction.js
@@ -0,0 +1,152 @@
+const cbor = require("cbor");
+const base58 = require("bs58");
+const { hex2buf, add256NoCarry, scalarAdd256ModM, multiply8 } = require("./utils");
+const crypto = require('crypto');
+
+exports.TxInput = class TxInput{
+  constructor(txId, outputIndex) {
+    this.id = txId;
+    this.outputIndex = outputIndex; // the index of the input transaction when it was the output of another
+    this.type = 0; // default input type
+  }
+
+  encodeCBOR(encoder) {
+    return encoder.pushAny([
+      this.type,
+      new cbor.Tagged(24, cbor.encode([
+        hex2buf(this.id),
+        this.outputIndex
+      ]))
+    ]);
+  }
+}
+
+exports.TxOutput = class TxOutput {
+  constructor(walletAddress, coins) {
+    this.walletAddress = walletAddress;
+    this.coins = coins;
+  }
+
+  encodeCBOR(encoder) {
+    return encoder.pushAny([this.walletAddress, this.coins]);
+  }
+}
+
+exports.WalletAddress =  class WalletAddress {
+  constructor(address) {
+    this.address = address;
+  }
+
+  encodeCBOR(encoder) {
+    return encoder.push(base58.decode(this.address));
+  }
+}
+
+exports.WalletSecretString = class WalletSecretString {
+    constructor (secretString) {
+        this.secretString = secretString;
+    }
+
+    getSecretKey() {
+        return this.secretString.substr(0,128);
+    }
+
+    getPublicKey() {
+        return this.secretString.substr(128, 64);
+    }
+
+    getChainCode() {
+        return this.secretString.substr(192, 64);
+    }
+}
+
+exports.TxPublicString = class TxPublicString {
+  // hex string representing 64 bytes
+  constructor(txPublicString) {
+    this.txPublicString = txPublicString;
+  }
+
+  getPublicKey() {
+    return this.txPublicString.substr(0, 64);
+  }
+
+  getChainCode() {
+    return this.txPublicString.substr(64, 64);
+  }
+
+  encodeCBOR(encoder) {
+    return encoder.pushAny(new Buffer(this.txPublicString, "hex"));
+  }
+}
+
+exports.TxSignature  = class TxSignature {
+  constructor(signature) {
+    this.signature = signature;
+  }
+
+  encodeCBOR(encoder) {
+    return encoder.pushAny(new Buffer(this.signature, "hex"));
+  }
+}
+
+exports.TxWitness = class TxWitness {
+  constructor(publicString, signature) {
+    this.publicString = publicString;
+    this.signature = signature;
+    this.type = 0; // default - PkWitness
+  }
+
+  getPublicKey() {
+    return this.publicString.getPublicKey();
+  }
+
+  getSignature() {
+    return this.signature.signature;
+  }
+
+  encodeCBOR(encoder) {
+    return encoder.pushAny([
+      this.type,
+      new cbor.Tagged(24, cbor.encode([this.publicString, this.signature]))
+    ]);
+  }
+}
+
+exports.deriveSK = function(parentSecretString, childIndex) {
+    var parentSecretString = new exports.WalletSecretString(parentSecretString);
+    var chainCode = new Buffer(parentSecretString.getChainCode(), 'hex');
+
+    var hmac1 = crypto.createHmac('sha512', chainCode);
+    hmac1.update(new Buffer('00', 'hex'));
+    hmac1.update(new Buffer(parentSecretString.getSecretKey(), 'hex'));
+    hmac1.update(new Buffer(childIndex.toString(16).padStart(8, '0'), 'hex'));
+    
+    var z = new Buffer(hmac1.digest('hex'), 'hex');
+
+    var zl8 = multiply8(z, new Buffer('08', 'hex')).slice(0,32);
+    var parentKey = new Buffer(parentSecretString.getSecretKey(), 'hex');
+
+    var kl = scalarAdd256ModM(zl8, parentKey.slice(0, 32));
+    var kr = add256NoCarry(z.slice(32, 64), parentKey.slice(32, 64));
+
+    var resKey = Buffer.concat([kl, kr]);
+
+    var hmac2 = crypto.createHmac('sha512', chainCode);
+    hmac2.update(new Buffer('01', 'hex'));
+    hmac2.update(new Buffer(parentSecretString.getSecretKey(), 'hex'));
+    hmac2.update(new Buffer(childIndex.toString(16).padStart(8, '0'), 'hex'));
+
+    var newChainCode = new Buffer(hmac2.digest('hex').slice(64, 128), 'hex');
+
+    /*
+    * TODO - to derive the public key, we need to import this C library:
+    * https://github.com/floodyberry/ed25519-donna
+    * more exactly, the function ed25519_publickey(secret_key, pub_key)
+    * which takes as an argument the secret key (its first 32 bytes) and returns
+    * the public key.
+    * Then we could also replace the function scalarAdd256ModM with ed25519_scalar_add
+    */
+    var newPublicKey = new Buffer(32);
+
+    return new exports.WalletSecretString(Buffer.concat([resKey, newPublicKey, newChainCode]).toString('hex'));
+}
