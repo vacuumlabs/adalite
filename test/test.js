2f9846484c67d6f0c3d53550ce05a2bc733d142a
refi93 <rafael.korbas@gmail.com>
2018-03-01 17:44:00 +0100

    refactored tests to mocha

--- test/test.js
+++ test/test.js
@@ -50,94 +50,112 @@ function mockRandomNumberGenerator(value) {
   sinon.stub(Math, 'random').returns(value);
 }
 
-exports["test signing"] = function() {
+describe("test signing", function() {
   var secret = new transaction.WalletSecretString("50f26a6d0e454337554274d703033c21a06fecfcb0457b15214e41ea3228ac51e2b9f0ca0f6510cfdd24325ac6676cdd98a9484336ba36c876fd93aa439d8b72eddaef2fab3d1412ea1f2517b5a50439c28c27d6aefafce38f9290c17e1e7d56c532f2e7a6620550b32841a24055e89c02256dec21d1f4418004ffc9591a8e9c");
   var message = "011a2d964a0958209585b64a94a56074504ad91121333b70b94027580b1e3bd49e18b541e8a4b950";
   var signature = "ca20e54f4cb12f0453de2d62b0ff041b0c90ef43e7f899c6cbc428dcd5bece2f68a9c8917e7e3881bf709b7845909dea8eb8bae46a1824f62fb80cc3b65aff02"
+  
+  it("should produce proper signature", function() {
+    // test signing
+    assert.equal(utils.sign(message, secret), signature);
+  });
+});
 
-  // test signing
-  assert.equal(utils.sign(message, secret), signature);
-}
-
-exports["test signature verification"] = function() {
+describe("test signature verification", function() {
   var secret = new transaction.WalletSecretString("50f26a6d0e454337554274d703033c21a06fecfcb0457b15214e41ea3228ac51e2b9f0ca0f6510cfdd24325ac6676cdd98a9484336ba36c876fd93aa439d8b72eddaef2fab3d1412ea1f2517b5a50439c28c27d6aefafce38f9290c17e1e7d56c532f2e7a6620550b32841a24055e89c02256dec21d1f4418004ffc9591a8e9c");
   var message = "011a2d964a0958209585b64a94a56074504ad91121333b70b94027580b1e3bd49e18b541e8a4b950";
   var rightSignature = "ca20e54f4cb12f0453de2d62b0ff041b0c90ef43e7f899c6cbc428dcd5bece2f68a9c8917e7e3881bf709b7845909dea8eb8bae46a1824f62fb80cc3b65aff02";
   var wrongSignature = "ca20e54f4cb12f0453de2d62b0ff041b0c90ef43e7f899c6cbc428dcd5bece2f68a9c8917e7e3881bf709b7845909dff8eb8bae46a1824f62fb80cc3b65aff02";
+  
+  it("should accept signature", function() {
+    assert.equal(utils.verify(message, secret.getPublicKey(), rightSignature), true);
+  });
 
-  assert.equal(utils.verify(message, secret.getPublicKey(), rightSignature), true);
-  assert.equal(utils.verify(message, secret.getPublicKey(), wrongSignature), false);
-}
+  it("should reject signature", function() {
+    assert.equal(utils.verify(message, secret.getPublicKey(), wrongSignature), false);
+  });
+});
 
-exports["test secret key derivation from mnemonic"] = function() {
+describe("test secret key derivation from mnemonic", function() {
   // a test case where the hash seed has an odd number of bytes
   var mnemonicString1 = "cruise bike bar reopen mimic title style fence race solar million clean";
   var generatedWalletSecret1 = mnemonic.mnemonicToWalletSecretString(mnemonicString1).secretString;
   var expectedWalletSecret1 = "b0d4187b81b5c2fb8234378ebcf33a1c2e2293369bd2263b6dcf672a29676a5a2e73d1f6e660365eacdde77052625f0cc6e50c0710b35e45095fb1b51b9b9315f83d8464268bbb19fe416000fa846eaed7171d4390242aa966ab80c36694b7fa6eec090fd6c6498bb4a28b61f8c4c5ae19b635e20052cb0bc7e0d17404b1717e";
-  assert.equal(generatedWalletSecret1, expectedWalletSecret1, "please check whether the seed has a leading zero - it shouldn't");
+  it("should produce right secret key from a seed which had a leading zero in hex by stripping it", function() {
+      assert.equal(generatedWalletSecret1, expectedWalletSecret1);
+  });
 
   // a test case where the hash seed has an even number of bytes
   var mnemonicString2 = "useful normal dismiss what earn total boost project tomorrow filter pill shuffle";
   var expectedWalletSecret2 = "30582ede015798e511207cb26d71ca460edb85a16fafe212261039eeaccd434fab1c009a83260352b8cf80241d097696d898b7a0a0296312227bb459c3784cc12770c30533d63e77ad46c26a47c1d659058ab0c3dcf0e899e40113e7def05dd73fc6f8b25d9d774caebaed348f8e1a7d503c958e0cf74337e95d1d5e4a2d4aa0";
   var generatedWalletSecret2 = mnemonic.mnemonicToWalletSecretString(mnemonicString2).secretString;
-  assert.equal(generatedWalletSecret2, expectedWalletSecret2);
+  it("should produce right secret key from a seed without a leading zero in hex", function() {
+      assert.equal(generatedWalletSecret2, expectedWalletSecret2);
+  });
+});
 
-}
 
-exports["test private key derivation"] = function() {
+describe("test private key derivation", function() {
   var secret = new transaction.WalletSecretString("a859bcad5de4fd8df3f3bfa24793dba52785f9a98832300844f028ff2dd75a5fcd24f7e51d3a2a72ac85cc163759b1103efb1d685308dcc6cd2cce09f70c948501e949b5b7a72f1ad304f47d842733b3481f2f096ca7ddfe8e1b7c20a1acafbb66ee772671d4fef6418f670e80ad44d1747a89d75a4ad386452ab5dc1acc32b3");
 
   // root public secret key (the one used as "wallet id" in Daedalus)
   var childIndex1 = 0x80000000;
   var expectedSecret1 = "28e375ee5af42a9641c5c31b1b2d24df7f1d2212116bc0b0fc58816f06985b072cf5960d205736cac2e8224dd6018f7223c1bdc630d2b866703670a37316f440de7c9d85c129d339dc4ac2fa6ea5f3a98606dcb38966b79fc3f361cb134654979661f96070a9d39df75c21f6142415502e254523cbacff2b4d58aa87d9021d65"
-  assert.equal(address.deriveSK(secret, childIndex1).secretString, expectedSecret1);
+  it("should properly derive root public secret key (the one used as \"wallet id\" in Daedalus)", function() {
+      assert.equal(address.deriveSK(secret, childIndex1).secretString, expectedSecret1);
+  });
 
   // some hardened secret key - child index starts with 1 in binary
   var childIndex2 = 0xf9745151;
   var expectedSecret2 = "ffd89a6ecc943cd58766294e7575d20f775eba62a93361412d61718026781c00d3d86147df3aa92147ea48f786b2cd2bd7d756d37add3055caa8ba4f1d543198e0bcbbda7b76ce5dc5dabb0728435e1031a21020248a23d36b74c094d6ad3ebcf0bd12bacfb4f58697cd088f6531130584933aed7dfe53163b7f24f10e6c25da";
-  assert.equal(address.deriveSK(secret, childIndex2).secretString, expectedSecret2);
+  it("should properly derive some hardened secret key - child index starts with 1 in binary", function() {
+    assert.equal(address.deriveSK(secret, childIndex2).secretString, expectedSecret2);
+  });
 
-  // some nonhardened secret key - child index starts with 0 in binary
   var childIndex3 = 0x10000323;
   var expectedSecret3 = "9f88124e740474c5575f090635050280ef65baf2c9836819c5f989afee884c005a8aabb3f7c26c277c7900c7bab232f682c0c17aa5a97fb1948f298589f65e722f42bccf4ee8fb814cc90299c2c6d7a12f4a38f35b5e50e55207ccc8984bd86cbb6af24fc5249eb317602d455cce7461112150afbfb66fb6525d3aba70fac7e4";
-  assert.equal(address.deriveSK(secret, childIndex3).secretString, expectedSecret3);
-}
+  it("should properly derive some nonhardened secret key - child index starts with 0 in binary", function() {
+    assert.equal(address.deriveSK(secret, childIndex3).secretString, expectedSecret3);
+  });
+});
 
-exports["test address generation from secret key"] = function() {
+describe("test address generation from secret key", function() {
   var secret = new transaction.WalletSecretString("a859bcad5de4fd8df3f3bfa24793dba52785f9a98832300844f028ff2dd75a5fcd24f7e51d3a2a72ac85cc163759b1103efb1d685308dcc6cd2cce09f70c948501e949b5b7a72f1ad304f47d842733b3481f2f096ca7ddfe8e1b7c20a1acafbb66ee772671d4fef6418f670e80ad44d1747a89d75a4ad386452ab5dc1acc32b3");
   
-  // root public address (the one used as "wallet id" in Daedalus)
   var childIndex1 = 0x80000000;
   var expectedAddress1 = "Ae2tdPwUPEZLdysXE34s6xRCpqSHvy5mRbrQiegSVQGQFBvkXf5pvseKuzH"
-  assert.equal(address.deriveAddressAndSecret(secret, childIndex1).address, expectedAddress1);
+  it("should properly generate root public address (the one used as \"wallet id\" in Daedalus)", function() {
+    assert.equal(address.deriveAddressAndSecret(secret, childIndex1).address, expectedAddress1);
+  });
 
-  // some hardened address - child index starts with 1 in binary
   var childIndex2 = 0xf9745151;
   var expectedAddress2 = "DdzFFzCqrhsoStdHaBGfa5ZaLysiTnVuu7SHRcJvvu4yKg94gVBx3TzEV9CjphrFxLhnu1DJUKm2kdcrxYDZBGosrv4Gq3HuiFWRYVdZ";
-  assert.equal(address.deriveAddressAndSecret(secret, childIndex2).address, expectedAddress2);
-  
-  // some nonhardened address - child index starts with 0 in binary
+  it("should properly generate some address from hardened key - child index starts with 1 in binary", function() {
+    assert.equal(address.deriveAddressAndSecret(secret, childIndex2).address, expectedAddress2);
+  });
+
   var childIndex3 = 0x10000323;
   var expectedAddress3 = "DdzFFzCqrhtBxUBQBKEUmVNYUQnnzgh46BhnxXnp4arQV2NF47gVqWpkFfSxnTmbjcznVrFVGquXAkaDwjT8AgBEZZQJvfZwtpox9czm";
-  assert.equal(address.deriveAddressAndSecret(secret, childIndex3).address, expectedAddress3);
-}
+  it("should properly generate some address from nonhardened key - child index starts with 0 in binary", function() {
+    assert.equal(address.deriveAddressAndSecret(secret, childIndex3).address, expectedAddress3);
+  });
+});
 
-exports["test address ownership verification"] = function() {
+describe("test address ownership verification", function() {
   var secret = new transaction.WalletSecretString("a859bcad5de4fd8df3f3bfa24793dba52785f9a98832300844f028ff2dd75a5fcd24f7e51d3a2a72ac85cc163759b1103efb1d685308dcc6cd2cce09f70c948501e949b5b7a72f1ad304f47d842733b3481f2f096ca7ddfe8e1b7c20a1acafbb66ee772671d4fef6418f670e80ad44d1747a89d75a4ad386452ab5dc1acc32b3");
 
   var ownAddress = "DdzFFzCqrhsoStdHaBGfa5ZaLysiTnVuu7SHRcJvvu4yKg94gVBx3TzEV9CjphrFxLhnu1DJUKm2kdcrxYDZBGosrv4Gq3HuiFWRYVdZ";
-  assert.equal(address.isAddressDerivableFromSecretString(ownAddress, secret), true);
+  it("should accept own address", function() {
+    assert.equal(address.isAddressDerivableFromSecretString(ownAddress, secret), true);
+  });
 
   var foreignAddress = "DdzFFzCqrht1Su7MEaCbFUcKpZnqQp5aUudPjrJZ2h8YADJBDvpsXZk9BducpXcSgujYJGKaTuZye9hb9z3Hff42TXDft5yrsKka6rDW"
-  assert.equal(address.isAddressDerivableFromSecretString(foreignAddress, secret), false);
-}
+  it("should reject foreign address", function() {
+    assert.equal(address.isAddressDerivableFromSecretString(foreignAddress, secret), false);
+  });
+});
 
-exports["test transaction fee function"] = async function() {
-  assert.equal(CardanoWallet.txFeeFunction(50), 157579);
-  assert.equal(CardanoWallet.txFeeFunction(351), 170807);
-}
-
-exports["test wallet addresses derivation"] = function() {
+describe("test wallet addresses derivation", function() {
   var expectedWalletAddresses = [
     "DdzFFzCqrht4XR8CKm4dPXikMyaSt6Y4iPvEwGYW7GYDgXyVHBbvRvGhzzEQT5XvZ3zVCJR7VB15PbVBzKeabPHruC3JvcFjFV8CynEG",
     "DdzFFzCqrht6dzaKMAgFmnkAiyDrdppXw9Pchpf5xofYxgogZTAETLCai7xFwWALmt1vZbwmF2oawDeapBYgkaECcZzYvUmHujuqTiAf",
@@ -162,33 +180,33 @@ exports["test wallet addresses derivation"] = function() {
   );
   var walletAddresses = wallet.getUsedAddresses();
 
-  assert.equal(JSON.stringify(walletAddresses), JSON.stringify(expectedWalletAddresses));
-}
+  it("should derive the right sequence of addresses from the root secret key", function() {
+    assert.equal(JSON.stringify(walletAddresses), JSON.stringify(expectedWalletAddresses));
+  });
+});
 
-exports["test transaction fee function"] = async function() {
-  assert.equal(CardanoWallet.txFeeFunction(50), 157579);
-  assert.equal(CardanoWallet.txFeeFunction(351), 170807);
-}
+describe("test transaction fee function", function() {
+  it("should properly compute transaction fee based on transaction size parameter", function() {
+    assert.equal(CardanoWallet.txFeeFunction(50), 157579);
+    assert.equal(CardanoWallet.txFeeFunction(351), 170807);
+  });
+});
 
-exports["test successful transaction fee computation"] = async function() {
+describe("test successful transaction fee computation", async function() {
   mockBlockChainExplorer();
   var wallet = new CardanoWallet(
     "A859BCAD5DE4FD8DF3F3BFA24793DBA52785F9A98832300844F028FF2DD75A5FCD24F7E51D3A2A72AC85CC163759B1103EFB1D685308DCC6CD2CCE09F70C948501E949B5B7A72F1AD304F47D842733B3481F2F096CA7DDFE8E1B7C20A1ACAFBB66EE772671D4FEF6418F670E80AD44D1747A89D75A4AD386452AB5DC1ACC32B3"
   );
+  it("should compute the right transaction fee for given transaction", async function() {
+    assert.equal(await wallet.getTxFee("DdzFFzCqrhsgPcpYL9aevEtfvP4bTFHde8kjT3acCkbK9SvfC9iikDPRtfRP8Sq6fsusNfRfm7sjhJfo7LDPT3c4rDr8PqkdHfW8PfuY", 47), 170938);
+  });
 
-  assert.equal(await wallet.getTxFee("DdzFFzCqrhsgPcpYL9aevEtfvP4bTFHde8kjT3acCkbK9SvfC9iikDPRtfRP8Sq6fsusNfRfm7sjhJfo7LDPT3c4rDr8PqkdHfW8PfuY", 47), 170938);
-}
+  it("should return fee -1 for an unallowed transaction (not enough resources on the account)", async function() {
+    assert.equal(await wallet.getTxFee("DdzFFzCqrhsgPcpYL9aevEtfvP4bTFHde8kjT3acCkbK9SvfC9iikDPRtfRP8Sq6fsusNfRfm7sjhJfo7LDPT3c4rDr8PqkdHfW8PfuY", 750000), -1);
+  });
+});
 
-exports["test unsuccessful transaction fee computation"] = async function() {
-  mockBlockChainExplorer();
-  var wallet = new CardanoWallet(
-    "A859BCAD5DE4FD8DF3F3BFA24793DBA52785F9A98832300844F028FF2DD75A5FCD24F7E51D3A2A72AC85CC163759B1103EFB1D685308DCC6CD2CCE09F70C948501E949B5B7A72F1AD304F47D842733B3481F2F096CA7DDFE8E1B7C20A1ACAFBB66EE772671D4FEF6418F670E80AD44D1747A89D75A4AD386452AB5DC1ACC32B3"
-  );
-
-  assert.equal(await wallet.getTxFee("DdzFFzCqrhsgPcpYL9aevEtfvP4bTFHde8kjT3acCkbK9SvfC9iikDPRtfRP8Sq6fsusNfRfm7sjhJfo7LDPT3c4rDr8PqkdHfW8PfuY", 750000), -1);
-}
-
-exports["test transaction serialization"] = async function() {
+describe("test transaction serialization", async function() {
   mockBlockChainExplorer();
   mockRandomNumberGenerator(0.7);
 
@@ -201,32 +219,41 @@ exports["test transaction serialization"] = async function() {
   // transaction serialization before providing witnesses
   var utxSerialized = cbor.encode(utx).toString("hex");
   var expectedUtxSerialized = "839f8200d81858248258203b8573d901522d73114b1c9671698d36b42931c863540fc699a636d3d93a1d6801ff9f8282d818584283581c13f3997560a5b81f5ac680b3322a2339433424e4e589ab3d752afdb6a101581e581c2eab4601bfe583febc23a04fb0abc21557adb47cea49c68d7b2f40a5001ac63884bf182f8282d818584283581cbbf99967ed781b4742106b6cf5c1cdfd4dcddb021e1709ad3f75a9f4a101581e581c2eab4601bfe583c2840b2b4fee027ac05963bc8f8f5ef30ddc77b4a1001adee0cecf1a0008d5c7ffa0";
-  assert.equal(utxSerialized, expectedUtxSerialized, "unsigned transaction body is wrong");
+  
+  it("should properly serialize transaction inner body", function() {
+    assert.equal(utxSerialized, expectedUtxSerialized)
+  });
 
   // transaction hash computation
   var txHash = utx.getId();
   var expectedTxHash = "b0a258bd8369ab409eab45c216fcddbd437821e2dff471ccb8b9f6e29e6ae29f";
-  assert.equal(txHash, expectedTxHash, "transaction hash is wrong");
+  it("should properly compute transaction hash", function() {
+    assert.equal(txHash, expectedTxHash, "transaction hash is wrong");
+  });
 
   // transaction witnesses computation
   var witnesses = utx.getWitnesses();
   var witnessesSerialized = cbor.encode(witnesses).toString("hex");
   var expectedWitnessesSerialized = "818200d8185885825840fa77e95ab9462cc64ff4499be26fdb2588a64102fc2dc07d8b9f3082d8fc3e5d494c450cdc2817cd0461a8df6625cbf69d5d767d91eac26e84f05436d57c937f58408d764dde1097d6690af2a8bf470090a853e877996cb08b019a97726c7eacb6027c6af3dd5afab5ac4a0e7230ff2560fcea1052fc07ad1c04121fb33ea7992b07";
-  assert.equal(witnessesSerialized, expectedWitnessesSerialized, "transaction witnesses are wrong");
+  it("should properly compute transaction witnesses", function() {
+    assert.equal(witnessesSerialized, expectedWitnessesSerialized, "transaction witnesses are wrong");
+  });
 
   // whole transaction serialization
   var txBody = cbor.encode(new transaction.SignedTransaction(utx, witnesses)).toString("hex");
   var expectedTxBody = "82839f8200d81858248258203b8573d901522d73114b1c9671698d36b42931c863540fc699a636d3d93a1d6801ff9f8282d818584283581c13f3997560a5b81f5ac680b3322a2339433424e4e589ab3d752afdb6a101581e581c2eab4601bfe583febc23a04fb0abc21557adb47cea49c68d7b2f40a5001ac63884bf182f8282d818584283581cbbf99967ed781b4742106b6cf5c1cdfd4dcddb021e1709ad3f75a9f4a101581e581c2eab4601bfe583c2840b2b4fee027ac05963bc8f8f5ef30ddc77b4a1001adee0cecf1a0008d5c7ffa0818200d8185885825840fa77e95ab9462cc64ff4499be26fdb2588a64102fc2dc07d8b9f3082d8fc3e5d494c450cdc2817cd0461a8df6625cbf69d5d767d91eac26e84f05436d57c937f58408d764dde1097d6690af2a8bf470090a853e877996cb08b019a97726c7eacb6027c6af3dd5afab5ac4a0e7230ff2560fcea1052fc07ad1c04121fb33ea7992b07";
-  assert.equal(txBody, expectedTxBody, "transaction serialization is wrong");
-}
+  it("should properly serialize the whole transaction", function() {
+    assert.equal(txBody, expectedTxBody, "transaction serialization is wrong");
+  });
+});
 
-exports["test wallet balance computation"] = async function() {
+describe("test wallet balance computation", async function() {
   mockBlockChainExplorer();
   var wallet = new CardanoWallet(
     "A859BCAD5DE4FD8DF3F3BFA24793DBA52785F9A98832300844F028FF2DD75A5FCD24F7E51D3A2A72AC85CC163759B1103EFB1D685308DCC6CD2CCE09F70C948501E949B5B7A72F1AD304F47D842733B3481F2F096CA7DDFE8E1B7C20A1ACAFBB66EE772671D4FEF6418F670E80AD44D1747A89D75A4AD386452AB5DC1ACC32B3"
   );
 
-  assert.equal(await wallet.getBalance(), 750000);
-}
-
-if (module == require.main) require("test").run(exports)
+  it("should properly fetch wallet balance", async function() {
+    assert.equal(await wallet.getBalance(), 750000);
+  });
+});
