7a7947153e7f3fc051dea03642a1906006b6f13d
refi93 <rafael.korbas@gmail.com>
2018-04-12 09:52:23 +0200

    moved browser check to server side

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const bowser = require('bowser')
 
 const config = require('./helpers/backendConfigLoader')
 
@@ -21,26 +22,77 @@ if (config.CARDANOLITE_FORCE_HTTPS === 'true') {
   })
 }
 
+const isBrowserSupported = (userAgent) => {
+  const minSupportedVersionsDesktop = {
+    chrome: '55',
+    chromium: '55',
+    firefox: '52',
+    safari: '11',
+    msedge: '15',
+    opera: '42',
+    msie: '12',
+  }
+
+  const minSupportedVersionsMobile = {
+    ios: '11.2',
+    chrome: '64',
+    android: '62',
+    msedge: '15',
+    opera: '42',
+    firefox: '57',
+  }
+
+  const isMobileDevice = bowser.mobile || bowser.tablet
+  const isDesktop = (isMobileDevice === undefined)
+
+  return (
+    (isDesktop && bowser.check(minSupportedVersionsDesktop, true, userAgent)) ||
+    (isMobileDevice && bowser.check(minSupportedVersionsMobile, true, userAgent))
+  )
+}
+
 app.get('/', (req, res) => {
-  return res.status(200).send(`
-    <!doctype html>
-    <html>
+  if (isBrowserSupported(req.headers['user-agent'])) {
+    return res.status(200).send(`
+      <!doctype html>
+      <html>
 
-      <head>
-        <title>CardanoLite Wallet</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="js/browserCompatibilityCheck.js" defer></script>
-        <script src="js/frontend.bundle.js" defer></script>
-        <link rel="stylesheet" type="text/css" href="css/styles.css">
-        <link rel="icon" type="image/ico" href="assets/favicon.ico">
-      </head>
+        <head>
+          <title>CardanoLite Wallet</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <script src="js/frontend.bundle.js" defer></script>
+          <link rel="stylesheet" type="text/css" href="css/styles.css">
+          <link rel="icon" type="image/ico" href="assets/favicon.ico">
+          <noscript>Your browser does not support JavaScript or it is turned off.</noscript>
+        </head>
 
-      <body data-config=${JSON.stringify(config)}>
-        <div id="root" style="width: 100%; height: 100%;"></div>
-      </body>
+        <body data-config=${JSON.stringify(config)}>
+          <div id="root" style="width: 100%; height: 100%;"></div>
+        </body>
 
-    </html>
-  `)
+      </html>
+    `)
+  } else {
+    return res.status(200).send(`
+      <!doctype html>
+      <html>
+
+        <head>
+          <title>CardanoLite Wallet</title>
+          <link rel="icon" type="image/ico" href="assets/favicon.ico">
+        </head>
+
+        <body>
+          Unsupported browser. Please try updating it or install the latest version of a supported one. We recommend trying:
+          <ul>
+            <li><a href="https://www.google.com/chrome">Google Chrome</a></li>
+            <li><a href="https://www.mozilla.org/en-US/firefox">Firefox</a></li>
+          </ul>
+        </body>
+
+      </html>
+    `)
+  }
 })
 
 require('./blockchainExplorerProxy')(app)
