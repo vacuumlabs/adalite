a0c9e71dd76c12b72f023a0917cc92ea21a22b87
Martin Pinter <martin.pinter@vacuumlabs.com>
2018-04-25 10:04:10 +0200

    Basic stats via redis

--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const express = require('express')
-const app = express()
-
+const redis = require('redis')
 const config = require('./helpers/backendConfigLoader')
+const app = express()
+const client = redis.createClient(config.REDIS_URL)
+
+const knownIps = new Set()
+
+const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -22,6 +40,19 @@ if (config.CARDANOLITE_FORCE_HTTPS === 'true') {
 
 app.use(express.static('public_wallet_app'))
 app.use(express.static('public_landing_page'))
+// stats middleware
+app.use((req, res, next) => {
+  // 'x-forwarded-for' due to internal heroku routing
+  const ip = req.headers['x-forwarded-for']
+  if (!knownIps.has(ip)) {
+    const date = new Date()
+    knownIps.add(ip)
+    client.incr('total')
+    client.incr(`${monthNames[date.getMonth()]}-${date.getFullYear()}`)
+    client.incr(`${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`)
+  }
+  next()
+})
 
 require('./blockchainExplorerProxy')(app)
 require('./transactionSubmitter')(app)
