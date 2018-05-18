f5a1f2c31cfbf882126a6c20236d1372e8c5b43e
jan_hreha <uni.jano@gmail.com>
2018-05-18 12:41:38 +0200

    scrollable disclaimer

--- frontend/components.js
+++ frontend/components.js
@@ -694,26 +694,30 @@ class AboutOverlayClass extends Component {
         }),
         h(
           'div',
-          {class: 'box text'},
-          h('h4', undefined, ' Disclaimer: CardanoLite is not created by Cardano Foundation. '),
+          {class: 'box'},
           h(
-            'p',
-            undefined,
-            `The official Cardano team did not review this code and is not responsible for any damage
+            'div',
+            {class: 'text'},
+            h('h4', undefined, ' Disclaimer: CardanoLite is not created by Cardano Foundation. '),
+            h(
+              'p',
+              undefined,
+              `The official Cardano team did not review this code and is not responsible for any damage
         it may cause you. The CardanoLite project is in alpha stage and should be used for
         penny-transactions only. We appreciate feedback, especially review of the crypto-related code.`
-          ),
-          h('h4', {class: 'header-margin'}, ' CardanoLite is not a bank '),
-          h(
-            'p',
-            undefined,
-            `
+            ),
+            h('h4', {class: 'header-margin'}, ' CardanoLite is not a bank '),
+            h(
+              'p',
+              undefined,
+              `
         It does not really store your funds permanently - each
         time you interact with it, you have to insert the mnemonic - the 12-words long root password
         to your account. If you lose it, we cannot help you restore the funds.
       `
+            ),
+            h('p', undefined, 'Feedback and contributions are very welcome.')
           ),
-          h('p', undefined, 'Feedback and contributions are very welcome.'),
           h(
             'label',
             {class: 'centered-row'},
