02dc2bda631f6054dc4695f5e8fd10a5cda53f66
Ján Hreha <uni.jano@gmail.com>
2018-05-18 16:43:08 +0200

    polish disclaimer on mobile devices (#70)
    scrollable disclaimer, padded action
    
    * Polish payment confirmation dialog

--- frontend/components.js
+++ frontend/components.js
@@ -350,7 +350,7 @@ const ConfirmTransactionDialog = connect(
     h(
       'div',
       {class: 'box'},
-      h('p', undefined, 'Confirm, that you really want to send'),
+      h('h4', undefined, 'Confirm, that you really want to send'),
       h(
         'div',
         undefined,
@@ -362,8 +362,12 @@ const ConfirmTransactionDialog = connect(
         ' to the address'
       ),
       h('div', {class: 'address-iniline'}, sendAddress),
-      h('button', {class: 'positive', onClick: submitTransaction}, 'Confirm'),
-      h('button', {class: 'danger', onClick: cancelTransaction}, 'Cancel')
+      h(
+        'div',
+        {class: 'centered-row'},
+        h('button', {class: 'positive', onClick: submitTransaction}, 'Confirm'),
+        h('button', {class: 'danger', onClick: cancelTransaction}, 'Cancel')
+      )
     )
   )
 )
@@ -694,36 +698,44 @@ class AboutOverlayClass extends Component {
         }),
         h(
           'div',
-          {class: 'box text'},
-          h('h4', undefined, ' Disclaimer: CardanoLite is not created by Cardano Foundation. '),
+          {class: 'box'},
           h(
-            'p',
-            undefined,
-            `The official Cardano team did not review this code and is not responsible for any damage
+            'div',
+            {class: 'message'},
+            h('h4', undefined, ' Disclaimer: CardanoLite is not created by Cardano Foundation. '),
+            h(
+              'p',
+              undefined,
+              `The official Cardano team did not review this code and is not responsible for any damage
         it may cause you. The CardanoLite project is in alpha stage and should be used for
         penny-transactions only. We appreciate feedback, especially review of the crypto-related code.`
-          ),
-          h('h4', {class: 'header-margin'}, ' CardanoLite is not a bank '),
-          h(
-            'p',
-            undefined,
-            `
+            ),
+            h('h4', {class: 'header-margin'}, ' CardanoLite is not a bank '),
+            h(
+              'p',
+              undefined,
+              `
         It does not really store your funds permanently - each
         time you interact with it, you have to insert the mnemonic - the 12-words long root password
         to your account. If you lose it, we cannot help you restore the funds.
       `
+            ),
+            h('p', undefined, 'Feedback and contributions are very welcome.')
           ),
-          h('p', undefined, 'Feedback and contributions are very welcome.'),
           h(
-            'label',
-            {class: 'centered-row'},
-            h('input', {
-              type: 'checkbox',
-              checked: dontShowAgainCheckbox,
-              onChange: this.checkboxClick,
-              class: 'understand-checkbox',
-            }),
-            'I understand the risk and do not wish to be shown this screen again'
+            'div',
+            undefined,
+            h(
+              'label',
+              {class: 'centered-row action'},
+              h('input', {
+                type: 'checkbox',
+                checked: dontShowAgainCheckbox,
+                onChange: this.checkboxClick,
+                class: 'understand-checkbox',
+              }),
+              'I understand the risk and do not wish to be shown this screen again'
+            )
           ),
           h(
             'span',
