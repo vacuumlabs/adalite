c64ea800814ab1171409610d2ba1067ae1b8109f
Dušan Plavák <dusan.plavak@gmail.com>
2018-06-25 19:10:58 +0200

    Refactor after PR

--- wallet/blockchain-explorer.js
+++ wallet/blockchain-explorer.js
@@ -42,7 +42,7 @@ const blockchainExplorer = (CARDANOLITE_CONFIG, walletState) => {
     const url = `${CARDANOLITE_CONFIG.CARDANOLITE_BLOCKCHAIN_EXPLORER_URL}/api/txs/raw/${txId}`
     const result = await request(url)
 
-    return JSON.parse(result.Right)
+    return new Buffer(JSON.parse(result.Right), 'hex')
   }
 
   async function getOverallTxCount(addresses) {
