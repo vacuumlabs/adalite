2dd9aa8a5ff01dee071a4d325db66703c6ebe4bf
refi93 <rafael.korbas@gmail.com>
2018-07-30 11:46:36 +0200

    removed star from trezor affiliate link

--- app/frontend/components/pages/login/hardwareAuth.js
+++ app/frontend/components/pages/login/hardwareAuth.js
@@ -2,7 +2,7 @@ const {h} = require('preact')
 
 const LoadByHardwareWalletSection = ({enableTrezor, loadWallet}) => {
   const TrezorAffiliateLink = (title) =>
-    h('a', {href: 'https://shop.trezor.io?a=cardanolite.com*', target: 'blank'}, title)
+    h('a', {href: 'https://shop.trezor.io?a=cardanolite.com', target: 'blank'}, title)
 
   const TrezorComingSoon = () =>
     h(
