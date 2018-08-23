1e6f2a2deeee0aacd43b05e7da40400c1daa91ad
refi93 <rafael.korbas@gmail.com>
2018-08-23 18:22:24 +0200

    derivation scheme V2 support for 15 word mnemonics

--- app/frontend/wallet/constants.js
+++ app/frontend/wallet/constants.js
@@ -2,21 +2,34 @@ module.exports = {
   HARDENED_THRESHOLD: 0x80000000,
   MAX_INT32: 2147483647,
   MAX_ADDRESS_INFO_AGE: 10000,
-  /*
-  * "011a2d964a095820" is a magic prefix from the cardano-sl code
-  * the "01" byte is a constant to denote signatures of transactions
-  * the "1a2d964a09" part is the CBOR representation of the blockchain-specific magic constant
-  * the "5820" part is the CBOR prefix for a hex string
-  */
-  TX_SIGN_MESSAGE_PREFIX: '011a2d964a095820',
 
-  // There are currently derivation modes 1 and 2. Daedalus uses derivation mode 1, so do we
-  CARDANO_KEY_DERIVATION_MODE: 1,
   BTC_BLOCKCHAIN_EXPLORER: 'https://www.blockchain.com/btc/address/',
   ETH_BLOCKCHAIN_EXPLORER: 'https://etherscan.io/address/',
   BTC_DONATION_ADDRESS: '3Qk3BDbw4yym6PM6vWA4bAsFkY76EWwtnp',
   ETH_DONATION_ADDRESS: '0xe1575549f79742d21E56426a1F9AD26997F5B9fb',
   ADA_DONATION_ADDRESS:
     'DdzFFzCqrhsqedBRRVa8dZ9eFQfQErikMsgJC2YkkLY23gK4JzV9y6jKnRL8VSDEqczdzG3WYmj1vsXxCA2j1MvTS6GfMVA2dkiFrkK5',
-  txWitnessByteSize: 139,
+  TX_WITNESS_SIZE_BYTES: 139,
+  NETWORKS: {
+    mainnet: {
+      protocolMagic: 764824073,
+    },
+    testnet: {
+      protocolMagic: 1097911063,
+    },
+  },
+  DERIVATION_SCHEMES: {
+    v1: {
+      type: 'v1',
+      number: 1,
+      startAddressIndex: 0x80000000,
+      defaultDerivationMode: 'hardened',
+    },
+    v2: {
+      type: 'v2',
+      number: 2,
+      startAddressIndex: 0,
+      defaultDerivationMode: 'nonhardened',
+    },
+  },
 }
--- app/frontend/wallet/mnemonic.js
+++ app/frontend/wallet/mnemonic.js
@@ -1,7 +1,5 @@
 const bip39 = require('bip39-light')
 
-const pbkdf2 = require('./helpers/pbkdf2')
-
 function generateMnemonic() {
   return bip39.generateMnemonic(null, null)
 }
@@ -25,17 +23,8 @@ function validateMnemonicWords(mnemonic) {
   }, true)
 }
 
-async function validatePaperWalletMnemonic(mnemonic) {
-  if (!!mnemonic && isMnemonicInPaperWalletFormat(mnemonic)) {
-    try {
-      await decodePaperWalletMnemonic(mnemonic)
-      return true
-    } catch (e) {
-      return false
-    }
-  }
-
-  return false
+function validatePaperWalletMnemonic(mnemonic) {
+  return !!mnemonic && validateMnemonicWords(mnemonic) && isMnemonicInPaperWalletFormat(mnemonic)
 }
 
 function isMnemonicInPaperWalletFormat(mnemonic) {
@@ -46,60 +35,9 @@ function mnemonicToList(mnemonic) {
   return mnemonic.split(' ')
 }
 
-async function mnemonicToPaperWalletPassphrase(mnemonic, password) {
-  const mnemonicBuffer = Buffer.from(mnemonic, 'utf8')
-  const salt = `mnemonic${password || ''}`
-  const saltBuffer = Buffer.from(salt, 'utf8')
-  return (await pbkdf2(mnemonicBuffer, saltBuffer, 2048, 32, 'sha512')).toString('hex')
-}
-
-async function decodePaperWalletMnemonic(paperWalletMnemonic) {
-  if (!validateMnemonicWords(paperWalletMnemonic)) {
-    throw Error('Invalid paper wallet mnemonic words')
-  }
-
-  const paperWalletMnemonicAsList = mnemonicToList(paperWalletMnemonic)
-
-  if (paperWalletMnemonicAsList.length !== 27) {
-    throw Error(
-      `Paper Wallet Mnemonic must be 27 words, got ${paperWalletMnemonicAsList.length} instead`
-    )
-  }
-
-  const mnemonicScrambledPart = paperWalletMnemonicAsList.slice(0, 18).join(' ')
-  const mnemonicPassphrasePart = paperWalletMnemonicAsList.slice(18, 27).join(' ')
-
-  const passphrase = await mnemonicToPaperWalletPassphrase(mnemonicPassphrasePart)
-  const unscrambledMnemonic = await unscrambleStrings(passphrase, mnemonicScrambledPart)
-
-  return unscrambledMnemonic
-}
-
-// eslint-disable-next-line max-len
-/* taken from https://github.com/input-output-hk/rust-cardano/blob/08796d9f100f417ff30549b297bd20b249f87809/cardano/src/paperwallet.rs */
-async function unscrambleStrings(passphrase, mnemonic) {
-  const input = Buffer.from(bip39.mnemonicToEntropy(mnemonic), 'hex')
-  const saltLength = 8
-
-  if (saltLength >= input.length) {
-    throw Error('unscrambleStrings: Input is too short')
-  }
-
-  const outputLength = input.length - saltLength
-
-  const output = await pbkdf2(passphrase, input.slice(0, saltLength), 10000, outputLength, 'sha512')
-
-  for (let i = 0; i < outputLength; i++) {
-    output[i] = output[i] ^ input[saltLength + i]
-  }
-
-  return bip39.entropyToMnemonic(output)
-}
-
 module.exports = {
   generateMnemonic,
   validateMnemonic,
   validatePaperWalletMnemonic,
   isMnemonicInPaperWalletFormat,
-  decodePaperWalletMnemonic,
 }
