2ce7635e816cdbf97db93f4d1bbfcd67b56f10c2
mirecmrozek <miroslav.mrozek@vacuumlabs.com>
2018-11-09 14:29:43 +0100

    format fiat amounts to be more readable

--- app/frontend/helpers/printConversionRates.js
+++ app/frontend/helpers/printConversionRates.js
@@ -1,13 +1,15 @@
+const addCommas = (amount) => parseFloat(amount).toLocaleString('en-US')
+
 const formatFiat = (amount, maxDigits) => {
   const newAmount = (amount * 0.000001).toFixed(2)
   const amountString = newAmount.toString()
   if (amountString.length <= maxDigits) {
-    return newAmount
+    return addCommas(newAmount)
   }
   if (amountString.indexOf('.') <= maxDigits + 1) {
-    return amountString.substr(0, maxDigits + 1)
+    return addCommas(amountString.substr(0, maxDigits + 1))
   }
-  return `${Math.trunc(amountString / 1000)}K`
+  return `${addCommas(Math.trunc(amountString / 1000))}K`
 }
 
 const printConversionRates = (amount, conversionRates, maxDigits) =>
@@ -16,10 +18,8 @@ const printConversionRates = (amount, conversionRates, maxDigits) =>
     : maxDigits
       ? `$\u00A0${formatFiat(conversionRates.USD * amount, maxDigits)},
         €\u00A0${formatFiat(conversionRates.EUR * amount, maxDigits)}`
-      : `$\u00A0${(conversionRates.USD * amount * 0.000001).toFixed(2)}, €\u00A0${(
-        conversionRates.EUR *
-          amount *
-          0.000001
-      ).toFixed(2)}`
+      : `$\u00A0${addCommas(
+        (conversionRates.USD * amount * 0.000001).toFixed(2)
+      )}, €\u00A0${addCommas((conversionRates.EUR * amount * 0.000001).toFixed(2))}`
 
 module.exports = printConversionRates
