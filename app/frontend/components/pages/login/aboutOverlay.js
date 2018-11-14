0d8aa46c7887b340993e53ecf3da74f59b001bf8
mirecmrozek <miroslav.mrozek@vacuumlabs.com>
2018-11-14 16:12:00 +0100

    update disclaimer text

--- app/frontend/components/pages/login/aboutOverlay.js
+++ app/frontend/components/pages/login/aboutOverlay.js
@@ -40,25 +40,92 @@ class AboutOverlayClass extends Component {
           h(
             'div',
             {class: 'message'},
-            h('h4', {class: 'text-center strong'}, ' Disclaimer '),
-            h('h4', undefined, ' AdaLite is not created by Cardano Foundation. '),
+            h('h4', {class: 'text-center strong'}, ' AdaLite is not a bank '),
             h(
               'p',
               undefined,
-              `The official Cardano team did not review this code and is not responsible for any damage
-        it may cause you. We appreciate feedback, especially review of the crypto-related code.`
+              h('b', undefined, 'AdaLite does not store your funds, mnemonic or private keys.'),
+              ' We are an open-source client-side interface for direct interaction with the Cardano blockchain.'
             ),
-            h('h4', {class: 'header-margin'}, ' AdaLite is not a bank '),
             h(
               'p',
               undefined,
-              `
-        It does not really store your funds permanently - each
-        time you interact with it, you have to insert the mnemonic - the 12-words long root password
-        to your account. If you lose it, we cannot help you restore the funds.
-      `
+              `You create a new wallet by generating a cryptographic set of words (mnemonic)
+              that are used to access your funds on the Cardano blockchain. The mnemonic is easy
+              to write down and more comfortable to remember representation of the wallet's
+              private key that is basically a root password for your wallet. `,
+              h(
+                'b',
+                undefined,
+                'If you lose your mnemonic, we cannot help you restore your funds.'
+              )
             ),
-            h('p', undefined, 'Feedback and contributions are very welcome.')
+            h(
+              'p',
+              undefined,
+              `Each time you want to access your wallet (interact with the blockchain)
+              through the AdaLite interface, you have to insert the mnemonic. `,
+              h(
+                'b',
+                undefined,
+                'Never share your mnemonic with anyone as it will allow them to access your funds!'
+              )
+            ),
+            h(
+              'p',
+              undefined,
+              'The mnemonic is handled in your browser and never leaves your computer. ',
+              h(
+                'b',
+                undefined,
+                `Nevertheless, if a virus or a hacker compromise your computer,
+              the attacker can steal the mnemonic when entered on AdaLite and access your funds.`
+              )
+            ),
+            h(
+              'p',
+              undefined,
+              'Therefore AdaLite offers you also the option to use ',
+              h(
+                'a',
+                {
+                  href:
+                      'https://shop.trezor.io/product/trezor-model-t?offer_id=15&aff_id=1071&source=Disclaimer',
+                  target: '_blank',
+                },
+                'Trezor model T'
+              ),
+              '. ',
+              h(
+                'b',
+                undefined,
+                `Trezor is a hardware wallet that will allow you to store your
+                mnemonic and private keys safely and interact with AdaLite in the safest possible
+                manner, without giving away to AdaLite's interface your mnemonic nor root private key.`
+              ),
+              ` Your private key will never leave the Trezor. Therefore an attacker can’t steal
+              them and gain control over your wallet.`
+            ),
+            h(
+              'p',
+              undefined,
+              'To protect yourself from phishers, bookmark the official AdaLite address and always check the URL ',
+              h('code', undefined, h('u', undefined, 'https://adalite.io/')),
+              '.'
+            ),
+            h(
+              'p',
+              undefined,
+              'AdaLite was not created by Cardano Foundation, Emurgo, or IOHK. This project was created with passion by ',
+              h('a', {href: 'https://vacuumlabs.com/', target: '_blank'}, 'Vacuum Labs'),
+              '. We appreciate any feedback, donation or contribution to the ',
+              h(
+                'a',
+                {href: 'https://github.com/vacuumlabs/adalite/', target: '_blank'},
+                'codebase'
+              ),
+              '.'
+            )
           ),
           h(
             'div',
