ea3ff896354b76dbec22d590642c61aceaa5efca
Jan Ferko <julyloov@gmail.com>
2018-12-14 15:01:08 +0100

    Change disclaimer content

--- app/frontend/components/pages/login/aboutOverlay.js
+++ app/frontend/components/pages/login/aboutOverlay.js
@@ -3,6 +3,15 @@ const connect = require('unistore/preact').connect
 const actions = require('../../../actions')
 const Modal = require('../../common/modal')
 
+const WelcomeRecommendation = ({children, title, containerClass, iconPath}) =>
+  h(
+    'div',
+    containerClass ? {class: containerClass} : undefined,
+    h('img', {src: iconPath}),
+    h('h5', undefined, title),
+    h('p', undefined, children)
+  )
+
 class AboutOverlayClass extends Component {
   constructor(props) {
     super(props)
@@ -28,116 +37,125 @@ class AboutOverlayClass extends Component {
       h(
         'div',
         {class: 'message'},
-        h('h4', {class: 'text-center strong'}, ' AdaLite is not a bank '),
+        h('h4', {class: 'text-center strong'}, 'Welcome to AdaLite'),
         h(
           'p',
-          undefined,
-          h('b', undefined, 'AdaLite does not store your funds, mnemonic or private keys.'),
-          ' We are an open-source client-side interface for direct interaction with the Cardano blockchain.'
+          {class: 'text-center'},
+          'We are an open-source client-side interface for direct interaction with the Cardano blockchain.'
         ),
+
         h(
-          'p',
-          undefined,
-          `You create a new wallet by generating a cryptographic set of words (mnemonic)
-              that are used to access your funds on the Cardano blockchain. The mnemonic is easy
-              to write down and more comfortable to remember representation of the wallet's
-              private key that is basically a root password for your wallet. `,
-          h('b', undefined, 'If you lose your mnemonic, we cannot help you restore your funds.')
-        ),
-        h(
-          'p',
-          undefined,
-          `Each time you want to access your wallet (interact with the blockchain)
-              through the AdaLite interface, you have to insert the mnemonic. `,
+          'div',
+          {class: 'alert warning'},
           h(
-            'b',
+            'p',
             undefined,
-            'Never share your mnemonic with anyone as it will allow them to access your funds!'
+            'To be safe from losing access to your funds, please read the following advice carefully.'
           )
         ),
+
         h(
-          'p',
+          'div',
           undefined,
-          'The mnemonic is handled in your browser and never leaves your computer. ',
           h(
-            'b',
-            undefined,
-            `Nevertheless, if a virus or a hacker compromise your computer,
-              the attacker can steal the mnemonic when entered on AdaLite and access your funds.`
-          )
-        ),
-        h(
-          'p',
-          undefined,
-          'Therefore AdaLite offers you also the option to use ',
-          h(
-            'a',
+            WelcomeRecommendation,
             {
-              href:
-                'https://shop.trezor.io/product/trezor-model-t?offer_id=15&aff_id=1071&source=Disclaimer',
-              target: '_blank',
+              title: "Don't loose your mnemonic",
+              // TODO iconPath: 'assets/mnemonic-icon.svg',
             },
-            'Trezor model T'
+            `A new wallet is created by generating a cryptographic set of words
+            (mnemonic). You use it to access your funds on the Cardano blockchain.
+            We don't store your mnemonic, and there is no way to reset it.
+            If you lose your mnemonic, we cannot help you to restore the access
+            to your funds.`
           ),
-          '. ',
           h(
-            'b',
-            undefined,
-            `Trezor is a hardware wallet that will allow you to store your
-                mnemonic and private keys safely and interact with AdaLite in the safest possible
-                manner, without giving away to AdaLite's interface your mnemonic nor root private key.`
-          ),
-          ` Your private key will never leave the Trezor. Therefore an attacker can’t steal
-              them and gain control over your wallet.`
-        ),
-        h(
-          'p',
-          undefined,
-          'To protect yourself from phishers, bookmark the official AdaLite address and always check the URL ',
-          h('code', undefined, h('u', undefined, 'https://adalite.io/')),
-          '.'
-        ),
-        h(
-          'p',
-          undefined,
-          'AdaLite was not created by Cardano Foundation, Emurgo, or IOHK. This project was created with passion by ',
-          h('a', {href: 'https://vacuumlabs.com/', target: '_blank'}, 'Vacuum Labs'),
-          '. We appreciate any feedback, donation or contribution to the ',
-          h('a', {href: 'https://github.com/vacuumlabs/adalite/', target: '_blank'}, 'codebase'),
-          '.'
-        )
-      ),
-      h('p', undefined, 'Feedback and contributions are very welcome.'),
-      h(
-        'div',
-        undefined,
-        h(
-          'label',
-          {class: 'centered-row action'},
-          h('input', {
-            type: 'checkbox',
-            checked: dontShowAgainCheckbox,
-            onChange: this.checkboxClick,
-            class: 'understand-checkbox',
-          }),
-          'I understand the risk and do not wish to be shown this screen again'
-        )
-      ),
-      h(
-        'span',
-        {class: 'centered-row'},
-        h(
-          'button',
-          {
-            onClick: this.toggleAboutOverlay,
-            class: 'rounded-button',
-            autofocus: true,
-            onKeyDown: (e) => {
-              e.key === 'Enter' && e.target.click()
-              e.key === 'Tab' && e.preventDefault()
+            WelcomeRecommendation,
+            {
+              title: 'Protect your funds',
+              // TODO iconPath: 'assets/shield-icon.svg',
             },
-          },
-          'Close'
+            `The mnemonic is handled in your browser and never leaves
+            your computer. However, if a virus or a hacker compromises your
+            computer, the attacker can steal the mnemonic you entered on
+            the AdaLite website and access the funds.`
+          )
+        ),
+        h(
+          'div',
+          undefined,
+          h(
+            WelcomeRecommendation,
+            {
+              title: 'Consider using a hardware wallet',
+              // TODO iconPath: 'assets/lock-icon.svg',
+            },
+            `AdaLite allows you to access your funds using a hardware wallet. It
+            currently supports Trezor model T. This allows you to interact with
+            AdaLite in the safest manner possible without giving away your
+            mnemonic. An attacker can't steal your mnemonic or private key since
+            they don't leave Trezor.`
+          ),
+          h(
+            WelcomeRecommendation,
+            {
+              title: "Don't get phished",
+              // TODO iconPath: 'assets/bookmark-icon.svg',
+            },
+            `To protect yourself from phishers, bookmark official AdaLite address
+            and `,
+            h('b', undefined, 'always check the URL. The official address is https://adalite.io/.')
+          )
+        ),
+
+        h(
+          'div',
+          undefined,
+          h('img', {src: 'assets/vacuumlabs-logo.svg'}),
+          h(
+            'p',
+            undefined,
+            `AdaLite was not created by Cardano Foundation, Emurgo, or IOHK.
+            This project was created with passion by Vacuumlabs. We appreciate
+            any feedback, donation or contribution to the codebase.`
+          )
+        ),
+
+        h(
+          'div',
+          undefined,
+          h(
+            'div',
+            undefined,
+            h(
+              'label',
+              {class: 'centered-row action'},
+              h('input', {
+                type: 'checkbox',
+                checked: dontShowAgainCheckbox,
+                onChange: this.checkboxClick,
+                class: 'understand-checkbox',
+              }),
+              "Don't show this notice again."
+            )
+          ),
+          h(
+            'span',
+            {class: 'centered-row'},
+            h(
+              'button',
+              {
+                onClick: this.toggleAboutOverlay,
+                class: 'rounded-button',
+                autofocus: true,
+                onKeyDown: (e) => {
+                  e.key === 'Enter' && e.target.click()
+                  e.key === 'Tab' && e.preventDefault()
+                },
+              },
+              'I understand, continue to the AdaLite'
+            )
+          )
         )
       )
     )
