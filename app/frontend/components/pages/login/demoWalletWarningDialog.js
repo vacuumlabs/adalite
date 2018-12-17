641eeb655434b4adcb42e631baf715384a881e47
David Novak <david.novak@hellephant.com>
2018-12-17 15:51:19 +0100

    Created layout for modal

--- app/frontend/components/pages/login/demoWalletWarningDialog.js
+++ app/frontend/components/pages/login/demoWalletWarningDialog.js
@@ -14,29 +14,39 @@ class DemoWalletWarningDialogClass {
       {closeHandler: closeDemoWalletWarningDialog},
       h(
         'div',
-        {class: 'center'},
-        h('h4', undefined, 'Warning'),
+        {class: 'modal-head'},
+        h('h2', {class: 'modal-title'}, 'Access demo wallet'),
+        h('div', {class: 'modal-warning'}, 'Proceed with caution')
+      ),
+      h(
+        'p',
+        {class: 'modal-paragraph'},
+        'You are about to access publicly available wallet intended to show public how AdaLite looks and works. ',
+        h('b', undefined, 'Your funds will not be safe here.')
+      ),
+      h('div', undefined, 'Alert will be here'),
+      h(
+        'div',
+        {class: 'modal-footer'},
         h(
-          'p',
-          undefined,
-          'You are opening the demo wallet which is publicly available. Do NOT use this wallet to store funds!'
+          'button',
+          {
+            class: 'button primary modal-button',
+            onClick: closeDemoWalletWarningDialog,
+            onKeyDown: (e) => {
+              ;['Enter', 'Escape'].includes(e.key) && e.target.click()
+            },
+            ref: (element) => {
+              this.understandDemoBtn = element
+            },
+          },
+          'I understand, continue to the demo wallet'
         ),
         h(
           'div',
-          {class: 'box-button-wrapper'},
-          h(
-            'button',
-            {
-              onClick: closeDemoWalletWarningDialog,
-              onKeyDown: (e) => {
-                ;['Enter', 'Escape'].includes(e.key) && e.target.click()
-              },
-              ref: (element) => {
-                this.understandDemoBtn = element
-              },
-            },
-            'I understand'
-          )
+          undefined,
+          'To securely store ADA, ',
+          h('a', {href: '/'}, 'create your own new wallet')
         )
       )
     )
