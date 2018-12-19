cc713817028f31d7f0c67a6bcd9131baf9173bd4
David Novak <david.novak@hellephant.com>
2018-12-19 10:50:43 +0100

    Refactored light <> dark states & implemented review

--- app/frontend/components/common/branding.js
+++ app/frontend/components/common/branding.js
@@ -1,16 +1,20 @@
 const {h} = require('preact')
 
-const WHITE_LOGO = 'assets/vacuumlabs-logo_white.svg'
-const DARK_LOGO = 'assets/vacuumlabs-logo.svg'
+const LOGO_PATH_WHITE = 'assets/vacuumlabs-logo_white.svg'
+const LOGO_PATH_DARK = 'assets/vacuumlabs-logo.svg'
 
-const Branding = ({inverse = false}) =>
+const Branding = ({dark}) =>
   h(
     'div',
     {
-      class: `branding ${inverse ? 'branding--inverse' : ''}`,
+      class: 'branding',
     },
-    h('p', {class: 'branding-label'}, 'Developed by'),
-    h('img', {class: 'branding-logo', src: inverse ? WHITE_LOGO : DARK_LOGO})
+    h('p', {class: `branding-label ${dark ? 'dark' : ''}`}, 'Developed by'),
+    h('img', {
+      class: 'branding-logo',
+      src: dark ? LOGO_PATH_DARK : LOGO_PATH_WHITE,
+      alt: 'Vacuumlabs logo',
+    })
   )
 
 module.exports = Branding
