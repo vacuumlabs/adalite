3098e4c4d5215b4e3d3dc89b42f36f0bdc1867a2
Dávid Novák <david.novak@vacuumlabs.com>
2018-12-21 09:51:50 +0100

    Demo wallet modal (#299)
    * Created layout for modal
    
    * Changed <b> to <strong>
    
    * Removed obsolete styling
    
    * Layout for demoWallet
    
    * Removed obsolete modal-button class
    
    * Refactored div to p in modal footer
    
    * Removed showCloseButton as its obsolete

--- app/frontend/components/pages/login/demoWalletWarningDialog.js
+++ app/frontend/components/pages/login/demoWalletWarningDialog.js
@@ -2,6 +2,7 @@ const {h} = require('preact')
 const connect = require('unistore/preact').connect
 const actions = require('../../../actions')
 const Modal = require('../../common/modal')
+const Alert = require('../../common/alert')
 
 class DemoWalletWarningDialogClass {
   componentDidMount() {
@@ -14,29 +15,49 @@ class DemoWalletWarningDialogClass {
       {closeHandler: closeDemoWalletWarningDialog},
       h(
         'div',
-        {class: 'center'},
-        h('h4', undefined, 'Warning'),
-        h(
-          'p',
-          undefined,
-          'You are opening the demo wallet which is publicly available. Do NOT use this wallet to store funds!'
-        ),
+        {class: 'modal-head'},
         h(
           'div',
-          {class: 'box-button-wrapper'},
+          undefined,
+          h('h2', {class: 'modal-title'}, 'Access demo wallet'),
           h(
-            'button',
-            {
-              onClick: closeDemoWalletWarningDialog,
-              onKeyDown: (e) => {
-                ;['Enter', 'Escape'].includes(e.key) && e.target.click()
-              },
-              ref: (element) => {
-                this.understandDemoBtn = element
-              },
-            },
-            'I understand'
+            'p',
+            {class: 'modal-paragraph'},
+            'You are about to access publicly available wallet intended to show public how AdaLite looks and works. ',
+            h('strong', undefined, 'Your funds will not be safe here.')
           )
+        ),
+        h('div', {class: 'modal-warning'}, 'Proceed with caution')
+      ),
+      h(
+        Alert,
+        {alertType: 'error'},
+        'All funds you will store to the demo wallet will be accessible to all AdaLite users. ',
+        h('strong', undefined, 'Don’t store your ADA to the demo wallet!')
+      ),
+      h(
+        'div',
+        {class: 'modal-footer'},
+        h(
+          'button',
+          {
+            class: 'button primary',
+            onClick: closeDemoWalletWarningDialog,
+            onKeyDown: (e) => {
+              ;['Enter', 'Escape'].includes(e.key) && e.target.click()
+            },
+            ref: (element) => {
+              this.understandDemoBtn = element
+            },
+          },
+          'I understand, continue to the demo wallet'
+        ),
+        /* TODO: connect link click to creating new wallet action */
+        h(
+          'p',
+          {class: 'modal-paragraph'},
+          'To securely store ADA, ',
+          h('a', {href: '/'}, 'create your own new wallet')
         )
       )
     )
