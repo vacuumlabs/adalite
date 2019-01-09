09b567258936abad4ad104d7f7393f31aa84d42b
David Novak <david.novak@hellephant.com>
2019-01-09 15:56:37 +0100

    Layout for logged-in navbar

--- app/frontend/components/common/navbar/navbarAuth.js
+++ app/frontend/components/common/navbar/navbarAuth.js
@@ -3,162 +3,73 @@ const connect = require('unistore/preact').connect
 const actions = require('../../../actions')
 const APP_VERSION = require('../../../config').ADALITE_CONFIG.ADALITE_APP_VERSION
 
-const LoginStatus = connect(
-  (state) => ({
-    pathname: state.router.pathname,
-  }),
-  actions
-)(({logout}) =>
-  h(
-    'div',
-    {class: 'status'},
-    h(
-      'label',
-      {class: 'inline on-desktop-only', for: 'navcollapse'},
-      h(
-        'div',
-        {class: 'button', onClick: () => setTimeout(logout, 100)},
-        h('img', {src: 'assets/exit-icon.svg'}),
-        h('div', {class: 'status-icon-button-content'}, 'Logout')
-      )
-    )
-  )
-)
-
 const NavbarAuth = connect(
   (state) => ({
-    pathname: state.router.pathname,
     isDemoWallet: state.isDemoWallet,
     showExportJsonWalletDialog: state.showExportJsonWalletDialog,
     usingTrezor: state.usingTrezor,
   }),
   actions
-)(({pathname, isDemoWallet, usingTrezor, logout}) => {
+)(({isDemoWallet, usingTrezor, logout}) => {
   const {
     history: {pushState},
   } = window
-  const currentTab = pathname.split('/')[1]
   return h(
-    'div',
-    {class: 'navbar'},
+    'nav',
+    {class: `navbar ${isDemoWallet ? 'demo' : ''}`},
     h(
       'div',
-      {class: 'navbar-wrap'},
+      {class: 'navbar-wrapper'},
       h(
-        'a',
-        {
-          class: 'title',
-          onClick: () => window.history.pushState({}, 'txHistory', 'txHistory'),
-        },
-        h('img', {src: 'assets/adalite-logo.svg', alt: 'AdaLite - Cardano Wallet'})
-      ),
-      isDemoWallet ? h('div', {class: 'public-wallet-badge pulse'}, 'DEMO WALLET') : null,
-      h('div', {class: 'logo-version-number'}, APP_VERSION),
-      h(
-        'div',
-        {class: 'on-mobile-only'},
+        'h1',
+        {class: 'navbar-heading'},
+        h('span', {class: 'navbar-title'}, 'AdaLite - Cardano Wallet'),
         h(
-          'div',
-          {class: 'centered-row'},
-          h(
-            'span',
-            {class: 'status'},
-            h(
-              'div',
-              {class: 'button', onClick: () => setTimeout(logout, 100)},
-              h('img', {src: 'assets/exit-icon.svg'}),
-              h('div', {class: 'status-icon-button-content'}, 'Logout')
-            )
-          ),
-          h(
-            'label',
-            {class: 'navcollapse-label', for: 'navcollapse'},
-            h('a', {class: 'menu-btn'}, h('img', {src: 'assets/menu-icon.svg'}))
-          )
+          'a',
+          {
+            class: 'navbar-logo',
+            href: '/',
+            onClick: () => window.history.pushState({}, 'txHistory', 'txHistory'),
+          },
+          h('img', {
+            src: 'assets/adalite-logo.svg',
+            alt: 'AdaLite - Cardano Wallet',
+          })
         )
       ),
-      h('input', {id: 'navcollapse', type: 'checkbox'}),
+      isDemoWallet && h('div', {class: 'navbar-demo'}, 'Accessing demo wallet'),
+      h('div', {class: 'navbar-version'}, `Ver. ${APP_VERSION}`),
       h(
-        'nav',
-        undefined,
+        'div',
+        {class: 'navbar-content'},
         h(
-          'div',
-          undefined,
-          h(
-            'label',
-            {class: 'inline', for: 'navcollapse'},
-            h(
-              'a',
-              {
-                class: currentTab === 'txHistory' && 'active',
-                onClick: () => pushState({}, 'txHistory', 'txHistory'),
-              },
-              'History'
-            )
-          ),
-          h(
-            'label',
-            {class: 'inline', for: 'navcollapse'},
-            h(
-              'a',
-              {
-                class: currentTab === 'send' && 'active',
-                onClick: () => pushState({}, 'send', 'send'),
-              },
-              'Send'
-            )
-          ),
-          h(
-            'label',
-            {class: 'inline', for: 'navcollapse'},
-            h(
-              'a',
-              {
-                class: currentTab === 'receive' && 'active',
-                onClick: () => pushState({}, 'receive', 'receive'),
-              },
-              'Receive'
-            )
-          ),
-          !usingTrezor &&
-            h(
-              'label',
-              {class: 'inline', for: 'navcollapse'},
-              h(
-                'a',
-                {
-                  class: currentTab === 'exportWallet' && 'active',
-                  onClick: () => pushState({}, 'exportWallet', 'exportWallet'),
-                },
-                'Export'
-              )
-            ),
-          h(
-            'label',
-            {class: 'inline', for: 'navcollapse'},
-            h(
-              'a',
-              {
-                href: 'about',
-                target: '_blank',
-              },
-              'About'
-            )
-          ),
-          h(
-            'label',
-            {class: 'inline', for: 'navcollapse'},
-            h(
-              'a',
-              {
-                href: 'https://github.com/vacuumlabs/adalite/wiki/AdaLite-FAQ',
-                target: '_blank',
-              },
-              'Help'
-            )
-          )
+          'a',
+          {
+            class: 'navbar-link',
+            href: 'about',
+            target: '_blank',
+            rel: 'noopener',
+          },
+          'About'
         ),
-        h(LoginStatus)
+        h(
+          'a',
+          {
+            class: 'navbar-link',
+            href: 'https://github.com/vacuumlabs/adalite/wiki/AdaLite-FAQ',
+            target: '_blank',
+            rel: 'noopener',
+          },
+          'Help'
+        ),
+        h(
+          'button',
+          {
+            class: 'button logout',
+            onClick: () => setTimeout(logout, 100),
+          },
+          'Logout'
+        )
       )
     )
   )
