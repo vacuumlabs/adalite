0aaf4313ed4fb6f01f4f6b099a4eb1114c403079
David Novak <david.novak@hellephant.com>
2019-01-10 15:08:20 +0100

    Layout & style address modal

--- app/frontend/components/common/addressDetailDialog.js
+++ app/frontend/components/common/addressDetailDialog.js
@@ -3,99 +3,29 @@ const QRious = require('../../libs/qrious')
 const connect = require('unistore/preact').connect
 const actions = require('../../actions')
 
-const Tooltip = require('./tooltip')
 const Modal = require('./modal')
-const CopyOnClick = require('./copyOnClick')
 
 class AddressDetailDialogClass extends Component {
-  componentDidUpdate() {
-    this.props.showDetail && this.copyBtn && this.copyBtn.focus()
-    // known bug: Trezor emulator steals the focus
-  }
-
-  render({showDetail, showVerification, error, verifyAddress, closeAddressDetail}) {
+  render({showDetail, closeAddressDetail}) {
     return (
       showDetail &&
       h(
         Modal,
         {
           closeHandler: closeAddressDetail,
+          bodyClass: 'narrow',
         },
-        h('b', undefined, 'Address:'),
         h(
           'div',
-          {class: 'full-address-row'},
-          h(
-            'span',
-            {
-              class: 'full-address force-select-all',
-            },
-            showDetail.address
-          )
-        ),
-        showVerification
-          ? h(
-            'div',
-            undefined,
-            h('b', undefined, 'Derivation path:'),
-            h(
-              'div',
-              {class: 'full-address-row'},
-              h('span', {class: 'full-address'}, showDetail.bip32path)
-            ),
-            h(
-              'div',
-              {class: 'text-center'},
-              h('button', {onClick: verifyAddress}, 'Verify on Trezor')
-            )
-          )
-          : h(
-            'div',
-            undefined,
-            h(
-              'div',
-              {class: 'centered-row'},
-              h('img', {
-                src: new QRious({
-                  value: showDetail.address,
-                  level: 'M',
-                  size: 200,
-                }).toDataURL(),
-              })
-            ),
-            h(
-              'div',
-              {class: 'centered-row'},
-              h(CopyOnClick, {
-                value: showDetail.address,
-                tabIndex: 0,
-                copyBtnRef: (element) => {
-                  this.copyBtn = element
-                },
-              }),
-              h(
-                Tooltip,
-                {tooltip: 'Examine via AdaScan.net'},
-                h(
-                  'a',
-                  {
-                    href: `https://adascan.net/address/${showDetail.address}`,
-                    target: '_blank',
-                    class: 'address-link margin-1rem centered-row',
-                    tabIndex: 0,
-                    onKeyDown: (e) => {
-                      e.key === 'Enter' && e.target.click()
-                      if (e.key === 'Tab') {
-                        this.copyBtn.focus()
-                        e.preventDefault()
-                      }
-                    },
-                  },
-                  h('img', {src: 'assets/link-icon.svg'})
-                )
-              )
-            )
-          )
+          {class: 'address-qr'},
+          h('img', {
+            src: new QRious({
+              value: showDetail.address,
+              level: 'M',
+              size: 200,
+            }).toDataURL(),
+          })
+        )
       )
     )
   }
@@ -104,8 +34,6 @@ class AddressDetailDialogClass extends Component {
 module.exports = connect(
   (state) => ({
     showDetail: state.showAddressDetail,
-    showVerification: state.showAddressVerification,
-    error: state.addressVerificationError,
   }),
   actions
 )(AddressDetailDialogClass)
