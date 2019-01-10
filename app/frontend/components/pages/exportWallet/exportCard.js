44def7a0015c4e3879704ec4852fefa608b4d1db
David Novak <david.novak@hellephant.com>
2019-01-10 16:46:17 +0100

    Layout for exportCard

---
+++ app/frontend/components/pages/exportWallet/exportCard.js
@@ -0,0 +1,28 @@
+const {h} = require('preact')
+
+const ExportCard = () =>
+  h(
+    'div',
+    {class: 'export card'},
+    h(
+      'div',
+      {class: 'export-head'},
+      h('h2', {class: 'export-title'}, 'Export your wallet key file'),
+      h('div', {class: 'export-type'}, 'JSON file')
+    ),
+    h(
+      'p',
+      {class: 'export-paragraph'},
+      'Key file can be used to access your wallet instead typing your mnemonic. It is encrypted by the password you choose.'
+    ),
+    h(
+      'button',
+      {
+        class: 'button primary outline fullwidth',
+        onClick: () => window.history.pushState({}, 'exportWallet', 'exportWallet'),
+      },
+      'Export key file'
+    )
+  )
+
+module.exports = ExportCard
