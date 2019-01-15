76e58c02072b9e7af1b9f09d165771649a39074b
Dávid Novák <david.novak@vacuumlabs.com>
2019-01-15 15:23:38 +0100

    Transactions component (#316)
    * Layout for transactions list
    
    * Styled transactions list
    
    * Added empty state for transactions listing
    
    * Refactored transactions colors
    
    * Added TODO comments
    
    * Implemented review comments
    
    * Adjusted balance-title after transaction-title refactor

--- app/frontend/components/pages/txHistory/transactionHistory.js
+++ app/frontend/components/pages/txHistory/transactionHistory.js
@@ -1,106 +1,76 @@
 const {h} = require('preact')
 const printAda = require('../../../helpers/printAda')
-const {printConversionRates} = require('../../../helpers/printConversionRates')
 const formatDate = require('../../../helpers/formatDate')
-const Tooltip = require('../../common/tooltip')
 
-const PrettyValue = ({effect}) => {
-  const value = printAda(Math.abs(effect))
-  const prefix = effect > 0 ? '+ ' : '- '
+const FormattedAmount = ({amount}) => {
+  const value = printAda(Math.abs(amount))
   const number = `${value}`.indexOf('.') === -1 ? `${value}.0` : `${value}`
   return h(
-    'pre',
-    {style: `color: ${effect > 0 ? 'green' : 'red'}`},
-    `${prefix}${number}`.padEnd(10)
+    'div',
+    {
+      class: `transaction-amount ${amount > 0 ? 'credit' : 'debit'}`,
+    },
+    `${number}`.padEnd(10)
+  )
+}
+
+const FormattedFee = ({fee}) => {
+  const value = printAda(fee)
+  return h(
+    'div',
+    {
+      class: 'transaction-fee',
+    },
+    `Fee: ${value}`
   )
 }
 
 const TransactionAddress = ({address}) =>
   h(
-    'a',
-    {
-      class: 'transaction-id with-tooltip',
-      tooltip: 'Examine via AdaScan.net',
-      href: `https://adascan.net/transaction/${address}`,
-      target: '_blank',
-    },
-    address
+    'div',
+    undefined,
+    h(
+      'a',
+      {
+        class: 'transaction-address',
+        href: `https://adascan.net/transaction/${address}`,
+        target: '_blank',
+        rel: 'noopener',
+      },
+      'View details'
+    )
   )
 
-const TransactionHistory = ({transactionHistory, conversionRates}) =>
+const TransactionHistory = ({transactionHistory}) =>
   h(
     'div',
-    {class: ''},
-    h('h2', undefined, 'Transaction History'),
-    h(
-      'div',
-      {class: 'transaction-history-wrapper'},
-      h(
-        'table',
-        {undefined},
-        h(
-          'thead',
-          undefined,
+    {class: 'transactions card'},
+    h('h2', {class: 'card-title'}, 'Transaction History'),
+    /* TODO: Load transaction items by chunks */
+    transactionHistory.length === 0
+      ? h('div', {class: 'transactions-empty'}, 'No transactions found')
+      : h(
+        'ul',
+        {class: 'transactions-content'},
+        ...transactionHistory.map((transaction) =>
           h(
-            'tr',
-            undefined,
-            h('th', undefined, 'Time'),
-            h(
-              'th',
-              undefined,
-              'Movement (',
-              h('img', {src: 'assets/ada-icon.svg', className: 'ada-sign-inline'}),
-              ')'
-            ),
-            h(
-              'th',
-              undefined,
-              'Fee (',
-              h('img', {src: 'assets/ada-icon.svg', className: 'ada-sign-inline'}),
-              ')'
-            ),
-            h('th', undefined, 'Transaction')
+            'li',
+            {class: 'transaction-item'},
+            h('div', {class: 'transaction-date'}, formatDate(transaction.ctbTimeIssued)),
+            h(FormattedAmount, {amount: transaction.effect}),
+            h(TransactionAddress, {address: transaction.ctbId}),
+            h(FormattedFee, {fee: transaction.fee})
           )
         ),
+        /* TODO: Implement button functionality */
         h(
-          'tbody',
-          undefined,
-          ...transactionHistory.map((transaction) =>
-            h(
-              'tr',
-              undefined,
-              h('td', undefined, formatDate(transaction.ctbTimeIssued)),
-              h(
-                'td',
-                {class: 'align-right'},
-                conversionRates
-                  ? h(
-                    Tooltip,
-                    {tooltip: `${printConversionRates(transaction.effect, conversionRates)}`},
-                    h(PrettyValue, {effect: transaction.effect})
-                  )
-                  : h(PrettyValue, {effect: transaction.effect})
-              ),
-              h(
-                'td',
-                {class: 'align-right'},
-                transaction.effect < 0
-                  ? conversionRates
-                    ? h(
-                      Tooltip,
-                      {tooltip: `${printConversionRates(transaction.fee, conversionRates)}`},
-                      h('pre', undefined, printAda(transaction.fee))
-                    )
-                    : h('pre', undefined, printAda(transaction.fee))
-                  : ''
-              ),
-              h('td', undefined, h(TransactionAddress, {address: transaction.ctbId}))
-            )
-          )
+          'button',
+          {
+            class: 'button primary fullwidth',
+          },
+          'Show another 8 transactions'
         )
-      ),
-      transactionHistory.length === 0 && h('div', undefined, 'No records')
-    )
+      )
   )
 
 module.exports = TransactionHistory
