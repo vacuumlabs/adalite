065fb98b9c50ec43a0cc0923e1b755d71e2c43e3
Dávid Novák <david.novak@vacuumlabs.com>
2019-01-16 17:51:10 +0100

    Login card content - mnemonic (#324)
    * Added showMnemonicInfoAlert action
    
    * Layout for login card mnemonic content
    
    * Styled login card mnemonic content
    
    * Fixed icon alignment in input for mnemonic
    
    * Fixed linter errors
    
    * Removed unnecessary styles

--- app/frontend/actions.js
+++ app/frontend/actions.js
@@ -175,6 +175,7 @@ module.exports = ({setState, getState}) => {
       mnemonicValidationError: undefined,
       showGenerateMnemonicDialog: true,
       authMethod: 'mnemonic',
+      showMnemonicInfoAlert: true,
     })
   }
 
