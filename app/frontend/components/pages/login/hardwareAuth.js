c84a5cb4987d9307ea546bb10770d6613db1a686
David Novak <david.novak@hellephant.com>
2019-03-08 09:09:30 +0100

    Styles and layout for Trezor model T image

--- app/frontend/components/pages/login/hardwareAuth.js
+++ app/frontend/components/pages/login/hardwareAuth.js
@@ -3,20 +3,29 @@ const {h} = require('preact')
 const LoadByHardwareWalletSection = ({loadWallet}) => {
   return h(
     'div',
-    {class: 'authentication-content centered'},
+    {class: 'authentication-content hardware'},
     h(
       'div',
-      {class: 'authentication-paragraph'},
-      'AdaLite supports Trezor model T (firmware version >= 2.0.8)'
+      undefined,
+      h(
+        'div',
+        {class: 'authentication-paragraph'},
+        'AdaLite supports Trezor model T (firmware version >= 2.0.8)'
+      ),
+      h(
+        'button',
+        {
+          class: 'button primary trezor',
+          onClick: () => loadWallet({cryptoProvider: 'trezor'}),
+        },
+        'Unlock with'
+      )
     ),
-    h(
-      'button',
-      {
-        class: 'button primary trezor',
-        onClick: () => loadWallet({cryptoProvider: 'trezor'}),
-      },
-      'Unlock with'
-    )
+    h('img', {
+      class: 'authentication-image',
+      src: 'assets/trezor.jpg',
+      alt: 'Trezor model T',
+    })
   )
 }
 
