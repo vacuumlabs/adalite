283780b36101a8100932e7f9975ff315a8cf5f05
David Novak <david.novak@hellephant.com>
2019-03-08 14:05:51 +0100

    Layout & styles for hw auth Ledger & Trezor T

--- app/frontend/components/pages/login/hardwareAuth.js
+++ app/frontend/components/pages/login/hardwareAuth.js
@@ -6,12 +6,14 @@ const LoadByHardwareWalletSection = ({loadWallet}) => {
     {class: 'authentication-content hardware'},
     h(
       'div',
-      undefined,
-      h(
-        'div',
-        {class: 'authentication-paragraph'},
-        'AdaLite supports Trezor model T (firmware version >= 2.0.8)'
-      ),
+      {class: 'authentication-wallet'},
+      h('img', {
+        class: 'authentication-image',
+        src: 'assets/trezor.jpg',
+        alt: 'Trezor model T',
+      }),
+      h('div', {class: 'authentication-paragraph'}, 'Trezor model T'),
+      h('div', {class: 'authentication-paragraph small'}, '(firmware version >= 2.0.8)'),
       h(
         'button',
         {
@@ -21,11 +23,45 @@ const LoadByHardwareWalletSection = ({loadWallet}) => {
         'Unlock with'
       )
     ),
-    h('img', {
-      class: 'authentication-image',
-      src: 'assets/trezor.jpg',
-      alt: 'Trezor model T',
-    })
+    h(
+      'div',
+      {class: 'authentication-wallet'},
+      h('img', {
+        class: 'authentication-image',
+        src: 'assets/ledger.jpg',
+        alt: 'Ledger Nano S',
+      }),
+      h('div', {class: 'authentication-paragraph'}, 'Ledger Nano S'),
+      h('div', {class: 'authentication-paragraph small'}, 'coming soon'),
+      h(
+        'button',
+        {
+          class: 'button grey ledger',
+          disabled: true,
+        },
+        'Unlock with'
+      )
+    ),
+    h(
+      'div',
+      {class: 'authentication-buttons'},
+      h(
+        'button',
+        {
+          class: 'button primary trezor',
+          onClick: () => loadWallet({cryptoProvider: 'trezor'}),
+        },
+        'Unlock with'
+      ),
+      h(
+        'button',
+        {
+          class: 'button grey ledger',
+          disabled: true,
+        },
+        'Unlock with'
+      )
+    )
   )
 }
 
