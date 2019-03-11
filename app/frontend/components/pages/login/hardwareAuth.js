122385d0d66dd1f16f0e6585b8ee3b8da0364bb2
refi93 <rafael.korbas@gmail.com>
2019-03-11 12:18:39 +0100

    disable ledger with a flag

--- app/frontend/components/pages/login/hardwareAuth.js
+++ app/frontend/components/pages/login/hardwareAuth.js
@@ -1,5 +1,8 @@
 const {h} = require('preact')
 const {CRYPTO_PROVIDER_TYPES} = require('../../../wallet/constants')
+const {
+  ADALITE_CONFIG: {DISABLE_LEDGER},
+} = require('../../../config')
 
 const LoadByHardwareWalletSection = ({loadWallet}) => {
   const TrezorAffiliateLink = (title) =>
@@ -10,6 +13,8 @@ const LoadByHardwareWalletSection = ({loadWallet}) => {
 
   // it doesn't work on Firefox even if U2F is enabled
   const isFirefox = navigator.userAgent.indexOf('Firefox') !== -1
+  const disableLedgerByFlag = DISABLE_LEDGER === 'true'
+  const disableLedger = disableLedgerByFlag || isFirefox
 
   return h(
     'div',
@@ -25,12 +30,23 @@ const LoadByHardwareWalletSection = ({loadWallet}) => {
       h(
         'div',
         {class: 'margin-top'},
-        'AdaLite supports Trezor model T and Ledger Nano S.',
-        h(
-          'a',
-          {href: 'https://github.com/vacuumlabs/adalite/wiki/Troubleshooting', target: 'blank'},
-          'Trouble connecting?'
-        )
+        `AdaLite supports Trezor model T and Ledger Nano S${
+          disableLedgerByFlag ? ' (coming soon!)' : '.'
+        }`,
+        disableLedgerByFlag
+          ? undefined
+          : h(
+            'div',
+            {class: 'margin-top'},
+            h(
+              'a',
+              {
+                href: 'https://github.com/vacuumlabs/adalite/wiki/Troubleshooting',
+                target: 'blank',
+              },
+              'Trouble connecting?'
+            )
+          )
       ),
       h(
         'div',
@@ -51,7 +67,7 @@ const LoadByHardwareWalletSection = ({loadWallet}) => {
           'button',
           {
             onClick: () => loadWallet({cryptoProviderType: CRYPTO_PROVIDER_TYPES.LEDGER}),
-            disabled: isFirefox,
+            disabled: disableLedger,
           },
           h(
             'div',
