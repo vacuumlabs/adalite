2b9c29927aa11dbea4e5fbc662118b3a6b27ff40
Dávid Novák <david.novak@vacuumlabs.com>
2019-03-12 07:20:58 +0100

    Trezor & Ledger HW Authentication tab (#408)
    * Trezor image exported
    
    * Styles and layout for Trezor model T image
    
    * Reexported Trezor image
    
    * Ledger image exported
    
    * Layout & styles for hw auth Ledger & Trezor T

--- app/frontend/components/pages/login/hardwareAuth.js
+++ app/frontend/components/pages/login/hardwareAuth.js
@@ -3,19 +3,64 @@ const {h} = require('preact')
 const LoadByHardwareWalletSection = ({loadWallet}) => {
   return h(
     'div',
-    {class: 'authentication-content centered'},
+    {class: 'authentication-content hardware'},
     h(
       'div',
-      {class: 'authentication-paragraph'},
-      'AdaLite supports Trezor model T (firmware version >= 2.0.8)'
+      {class: 'authentication-wallet'},
+      h('img', {
+        class: 'authentication-image',
+        src: 'assets/trezor.jpg',
+        alt: 'Trezor model T',
+      }),
+      h('div', {class: 'authentication-paragraph'}, 'Trezor model T'),
+      h('div', {class: 'authentication-paragraph small'}, '(firmware version >= 2.0.8)'),
+      h(
+        'button',
+        {
+          class: 'button primary trezor',
+          onClick: () => loadWallet({cryptoProvider: 'trezor'}),
+        },
+        'Unlock with'
+      )
     ),
     h(
-      'button',
-      {
-        class: 'button primary trezor',
-        onClick: () => loadWallet({cryptoProvider: 'trezor'}),
-      },
-      'Unlock with'
+      'div',
+      {class: 'authentication-wallet'},
+      h('img', {
+        class: 'authentication-image',
+        src: 'assets/ledger.jpg',
+        alt: 'Ledger Nano S',
+      }),
+      h('div', {class: 'authentication-paragraph'}, 'Ledger Nano S'),
+      h('div', {class: 'authentication-paragraph small'}, 'coming soon'),
+      h(
+        'button',
+        {
+          class: 'button grey ledger',
+          disabled: true,
+        },
+        'Unlock with'
+      )
+    ),
+    h(
+      'div',
+      {class: 'authentication-buttons'},
+      h(
+        'button',
+        {
+          class: 'button primary trezor',
+          onClick: () => loadWallet({cryptoProvider: 'trezor'}),
+        },
+        'Unlock with'
+      ),
+      h(
+        'button',
+        {
+          class: 'button grey ledger',
+          disabled: true,
+        },
+        'Unlock with'
+      )
     )
   )
 }
