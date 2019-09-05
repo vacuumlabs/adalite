28d98bf228cc2cb1f727a74dd7a5c2ebcd2c8b96
xdzurman <kamil.dzurman@gmail.com>
2019-09-05 12:25:06 +0200

    Add back button functionality to custom input

---
+++ app/frontend/components/pages/sendAda/customInputButton.js
@@ -0,0 +1,41 @@
+const {h} = require('preact')
+const connect = require('unistore/preact').connect
+const actions = require('../../../actions')
+
+const CustomInputButton = ({
+  isSendAddressValid,
+  toggleCustomDonation,
+  setDonation,
+  donationAmount,
+  maxDonationAmount,
+}) => {
+  const maxDonationAmountInAda = Math.floor(maxDonationAmount * 0.001) / 1000
+
+  return donationAmount.coins > maxDonationAmount
+    ? h(
+      'button',
+      {
+        class: 'button send-max',
+        onClick: setDonation,
+        disabled: !isSendAddressValid,
+        value: maxDonationAmountInAda,
+      },
+      `Max (${maxDonationAmountInAda})`
+    )
+    : h(
+      'button',
+      {
+        class: 'button send-max',
+        onClick: toggleCustomDonation,
+      },
+      'Back'
+    )
+}
+
+module.exports = connect(
+  (state) => ({
+    donationAmount: state.donationAmount,
+    maxDonationAmount: state.maxDonationAmount,
+  }),
+  actions
+)(CustomInputButton)
