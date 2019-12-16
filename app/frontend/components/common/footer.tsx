632e5d1b2e1810a7a2a61121de4b289ce5564a29
Peter Peresini <peter.peresini@gmail.com>
2019-12-16 09:30:00 +0100

    Refactor - change Footer to use hooks

--- app/frontend/components/common/footer.tsx
+++ app/frontend/components/common/footer.tsx
@@ -1,5 +1,5 @@
 import {h} from 'preact'
-import {connect} from '../../helpers/connect'
+import {useSelector, useActions} from '../../helpers/connect'
 import actions from '../../actions'
 import Branding from './branding'
 import {
@@ -17,91 +17,93 @@ interface Props {
 
 const showRatesOn = ['/txHistory', '/send']
 
-const Footer = connect(
-  (state) => ({
-    showConversionRates: showRatesOn.indexOf(state.router.pathname) !== -1 && state.walletIsLoaded,
-  }),
-  actions
-)(({showConversionRates, showContactFormModal}: Props) => (
-  <footer className="footer">
-    <div className="footer-wrapper">
-      <Branding dark={false} />
-      <div className="footer-row">
-        <div className="social">
-          <button role="button" className="social-link email" onClick={showContactFormModal}>
-            Contact us
-          </button>
-          <a
-            href="https://t.me/AdaLite"
-            target="_blank"
-            rel="noopener"
-            className="social-link telegram"
-          >
-            Telegram
-          </a>
-          <a
-            href="https://github.com/vacuumlabs/adalite"
-            target="_blank"
-            rel="noopener"
-            className="social-link github"
-          >
-            View on Github
-          </a>
-          <a
-            href="https://twitter.com/AdaLiteWallet"
-            target="_blank"
-            rel="noopener"
-            className="social-link twitter"
-          >
-            Twitter
-          </a>
+const Footer = () => {
+  const showConversionRates = useSelector(
+    (state) => showRatesOn.indexOf(state.router.pathname) !== -1 && state.walletIsLoaded
+  )
+  const {showContactFormModal} = useActions(actions)
+
+  return (
+    <footer className="footer">
+      <div className="footer-wrapper">
+        <Branding dark={false} />
+        <div className="footer-row">
+          <div className="social">
+            <button role="button" className="social-link email" onClick={showContactFormModal}>
+              Contact us
+            </button>
+            <a
+              href="https://t.me/AdaLite"
+              target="_blank"
+              rel="noopener"
+              className="social-link telegram"
+            >
+              Telegram
+            </a>
+            <a
+              href="https://github.com/vacuumlabs/adalite"
+              target="_blank"
+              rel="noopener"
+              className="social-link github"
+            >
+              View on Github
+            </a>
+            <a
+              href="https://twitter.com/AdaLiteWallet"
+              target="_blank"
+              rel="noopener"
+              className="social-link twitter"
+            >
+              Twitter
+            </a>
+          </div>
+          <div className="donations">
+            <h3 className="donations-title">Donations are appreciated</h3>
+            <a
+              className="donations-item bitcoin"
+              href={BTC_BLOCKCHAIN_EXPLORER + BTC_DONATION_ADDRESS}
+              target="_blank"
+              title="Donate via Bitcoin"
+              rel="noopener"
+            >
+              BTC
+            </a>
+            <a
+              className="donations-item ether"
+              href={ETH_BLOCKCHAIN_EXPLORER + ETH_DONATION_ADDRESS}
+              target="_blank"
+              title="Donate via Ethereum"
+              rel="noopener"
+            >
+              ETH
+            </a>
+            <a
+              className="donations-item ada"
+              href={`https://seiza.com/blockchain/address/${ADA_DONATION_ADDRESS}`}
+              target="_blank"
+              title="Donate via Adalite"
+              rel="noopener"
+            >
+              ADA
+            </a>
+          </div>
         </div>
-        <div className="donations">
-          <h3 className="donations-title">Donations are appreciated</h3>
-          <a
-            className="donations-item bitcoin"
-            href={BTC_BLOCKCHAIN_EXPLORER + BTC_DONATION_ADDRESS}
-            target="_blank"
-            title="Donate via Bitcoin"
-            rel="noopener"
-          >
-            BTC
-          </a>
-          <a
-            className="donations-item ether"
-            href={ETH_BLOCKCHAIN_EXPLORER + ETH_DONATION_ADDRESS}
-            target="_blank"
-            title="Donate via Ethereum"
-            rel="noopener"
-          >
-            ETH
-          </a>
-          <a
-            className="donations-item ada"
-            href={`https://seiza.com/blockchain/address/${ADA_DONATION_ADDRESS}`}
-            target="_blank"
-            title="Donate via Adalite"
-            rel="noopener"
-          >
-            ADA
-          </a>
-        </div>
+        {showConversionRates && (
+          <div className="conversion">
+            Conversion rates from{' '}
+            <a
+              className="conversion-link"
+              href="https://www.cryptocompare.com/api/"
+              target="_blank"
+              rel="noopener"
+            >
+              CryptoCompare
+            </a>
+          </div>
+        )}
       </div>
-      {showConversionRates && (
-        <div className="conversion">
-          Conversion rates from{' '}
-          <a
-            className="conversion-link"
-            href="https://www.cryptocompare.com/api/"
-            target="_blank"
-            rel="noopener"
-          >
-            CryptoCompare
-          </a>
-        </div>
-      )}
-    </div>
-  </footer>
-))
+    </footer>
+  )
+}
 
 export default Footer
