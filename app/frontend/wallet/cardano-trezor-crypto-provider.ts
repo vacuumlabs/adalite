vacuumlabs/adalite

commit cf2a2e55373e406098cede122eb6358bc9dfc998

Renamed most of js->{tsx,ts} and fixed immediate problems

app/frontend/wallet/cardano-trezor-crypto-provider.ts
@@ -1,0 +1,152 @@
+// eslint-disable-next-line import/no-unresolved
+import CachedDeriveXpubFactory from './helpers/CachedDeriveXpubFactory'
+import {ADALITE_SUPPORT_EMAIL} from './constants'
+import derivationSchemes from './derivation-schemes'
+import NamedError from '../helpers/NamedError'
+import debugLog from '../helpers/debugLog'
+
+const CardanoTrezorCryptoProvider = (ADALITE_CONFIG, walletState) => {
+  const state = Object.assign(walletState, {
+    rootHdPassphrase: null,
+    derivedAddresses: {},
+    derivationScheme: derivationSchemes.v2,
+  })
+
+  const TrezorConnect = ADALITE_CONFIG.ADALITE_TREZOR_CONNECT_URL
+    ? (window as any).TrezorConnect
+    : require('trezor-connect').default
+
+  TrezorConnect.manifest({
+    email: ADALITE_SUPPORT_EMAIL,
+    appUrl: ADALITE_CONFIG.ADALITE_SERVER_URL,
+  })
+
+  const isHwWallet = () => true
+  const getHwWalletName = () => 'Trezor'
+
+  const deriveXpub = CachedDeriveXpubFactory(state.derivationScheme, async (absDerivationPath) => {
+    const response = await TrezorConnect.cardanoGetPublicKey({
+      path: absDerivationPath,
+      showOnTrezor: false,
+    })
+    throwIfNotSuccess(response)
+    return Buffer.from(response.payload.publicKey, 'hex')
+  })
+
+  function deriveHdNode(childIndex) {
+    throw NamedError(
+      'UnsupportedOperationError',
+      'This operation is not supported on TrezorCryptoProvider!'
+    )
+  }
+
+  function sign(message, absDerivationPath) {
+    throw NamedError('UnsupportedOperationError', 'Operation not supported')
+  }
+
+  async function displayAddressForPath(absDerivationPath) {
+    const response = await TrezorConnect.cardanoGetAddress({
+      path: absDerivationPath,
+      showOnTrezor: true,
+    })
+
+    throwIfNotSuccess(response)
+
+    state.derivedAddresses[JSON.stringify(absDerivationPath)] = {
+      derivationPath: absDerivationPath,
+      address: response.payload.address,
+    }
+    return response.payload.address
+  }
+
+  function prepareInput(input, addressToAbsPathMapper) {
+    const data = {
+      prev_hash: input.txHash,
+      prev_index: input.outputIndex,
+      type: 0,
+      path: addressToAbsPathMapper(input.utxo.address),
+    }
+
+    return data
+  }
+
+  function prepareOutput(output, addressToAbsPathMapper) {
+    const data = {
+      amount: `${output.coins}`,
+      path: undefined,
+      address: undefined,
+    }
+
+    if (output.isChange) {
+      data.path = addressToAbsPathMapper(output.address)
+    } else {
+      data.address = output.address
+    }
+
+    return data
+  }
+
+  async function signTx(unsignedTx, rawInputTxs, addressToAbsPathMapper) {
+    const inputs = []
+    for (const input of unsignedTx.inputs) {
+      inputs.push(await prepareInput(input, addressToAbsPathMapper))
+    }
+
+    const outputs = []
+    for (const output of unsignedTx.outputs) {
+      const data = await prepareOutput(output, addressToAbsPathMapper)
+      outputs.push(data)
+    }
+
+    const transactions = rawInputTxs.map((tx) => tx.toString('hex'))
+
+    const response = await TrezorConnect.cardanoSignTransaction({
+      inputs,
+      outputs,
+      transactions,
+      protocol_magic: state.network.protocolMagic,
+    })
+
+    if (response.error || !response.success) {
+      debugLog(response)
+      throw NamedError('TrezorSignTxError', response.payload.error)
+    }
+
+    return {
+      txHash: response.payload.hash,
+      txBody: response.payload.body,
+    }
+  }
+
+  function getWalletSecret() {
+    throw NamedError('UnsupportedOperationError', 'Unsupported operation!')
+  }
+
+  function getDerivationScheme() {
+    return state.derivationScheme
+  }
+
+  function throwIfNotSuccess(response) {
+    if (response.error || !response.success) {
+      debugLog(response)
+      throw NamedError(
+        'TrezorError',
+        'Trezor operation failed, please make sure ad blockers are switched off for this site'
+      )
+    }
+  }
+
+  return {
+    getWalletSecret,
+    getDerivationScheme,
+    signTx,
+    displayAddressForPath,
+    deriveXpub,
+    isHwWallet,
+    getHwWalletName,
+    _sign: sign,
+    _deriveHdNode: deriveHdNode,
+  }
+}
+
+export default CardanoTrezorCryptoProvider

commit 9c85f26e90d544b51c917cc47d751c0edda64df9

Fix regression with Trezor signing It was introduced in cf2a2e55373e406098cede122eb6358bc9dfc998

app/frontend/wallet/cardano-trezor-crypto-provider.ts
@@ -70,11 +70,15 @@ const CardanoTrezorCryptoProvider = (ADALITE_CONFIG, walletState) => {
     return data
   }
 
+  interface TrezorOutput {
+    amount: string
+    address?: string
+    path?: Array<number>
+  }
+
   function prepareOutput(output, addressToAbsPathMapper) {
-    const data = {
+    const data: TrezorOutput = {
       amount: `${output.coins}`,
-      path: undefined,
-      address: undefined,
     }
 
     if (output.isChange) {