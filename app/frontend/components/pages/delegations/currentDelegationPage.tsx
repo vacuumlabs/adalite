96e8c9c35e2621c2e04eb8c852a4214a94fc9c56
PeterBenc <peter.benc1@gmail.com>
2020-02-06 19:32:26 +0100

    fix no delegation page

--- app/frontend/components/pages/delegations/currentDelegationPage.tsx
+++ app/frontend/components/pages/delegations/currentDelegationPage.tsx
@@ -7,42 +7,42 @@ const CurrentDelegationPage = ({currentDelegation, revokeDelegation}) => {
   return (
     <div className="current-delegation card">
       <h2 className="card-title">Current delegation</h2>
-      {currentDelegation ? (
-        <div className="current-delegation-wrapper">
-          {/* <div className="delegation-history-time">
-            {formatDelegationDate(currentDelegation[0].time)}
+      {currentDelegation.length ? (
+        <div>
+          <div className="current-delegation-wrapper">
+            {/* <div className="delegation-history-time">
+              {formatDelegationDate(currentDelegation[0].time)}
+            </div>
+            <div /> */}
+            {currentDelegation.map((pool) => [
+              <div className="delegation-history-name">{pool.name}</div>,
+              <div className="delegation-history-percent">{`${pool.ratio} %`}</div>,
+              <div className="delegation-history-id">{pool.pool_id}</div>,
+              <div />,
+              <div className="delegation-history-id">{`Ticker: ${pool.ticker}`}</div>,
+              <div />,
+              <div className="delegation-history-id">
+                {`
+                Tax: ${(pool.rewards.ratio[0] * 100) / pool.rewards.ratio[1] || ''}%
+                ${pool.rewards.fixed ? ` , ${`Fixed: ${printAda(pool.rewards.fixed)}`}` : ''}
+                ${pool.rewards.limit ? ` , ${`Limit: ${printAda(pool.rewards.limit)}`}` : ''}
+              `}
+              </div>,
+              <div />,
+              <div className="delegation-history-id">
+                {'Homepage: '}
+                <a href={pool.homepage}>{pool.homepage}</a>
+              </div>,
+              <div />,
+            ])}
           </div>
-          <div /> */}
-          {currentDelegation.map((pool) => [
-            <div className="delegation-history-name">{pool.name}</div>,
-            <div className="delegation-history-percent">{`${pool.ratio} %`}</div>,
-            <div className="delegation-history-id">{pool.pool_id}</div>,
-            <div />,
-            <div className="delegation-history-id">{`Ticker: ${pool.ticker}`}</div>,
-            <div />,
-            <div className="delegation-history-id">
-              {`
-              Tax: ${(pool.rewards.ratio[0] * 100) / pool.rewards.ratio[1] || ''}%
-              ${pool.rewards.fixed ? ` , ${`Fixed: ${printAda(pool.rewards.fixed)}`}` : ''}
-              ${pool.rewards.limit ? ` , ${`Limit: ${printAda(pool.rewards.limit)}`}` : ''}
-            `}
-            </div>,
-            <div />,
-            <div className="delegation-history-id">
-              {'Homepage: '}
-              <a href={pool.homepage}>{pool.homepage}</a>
-            </div>,
-            <div />,
-          ])}
+          <button className="button primary revoke-delegation" onClick={revokeDelegation}>
+            Revoke current delegation
+          </button>
         </div>
       ) : (
-        <button className="button stake-pool" onClick={null}>
-          The funds are currently undelegated. Delegate now.
-        </button>
+        <p>The funds are currently undelegated. Delegate now.</p>
       )}
-      <button className="button primary revoke-delegation" onClick={revokeDelegation}>
-        Revoke current delegation
-      </button>
     </div>
   )
 }
