3bfc292bfb8887e2c6523a67f36f7381d747a4c3
xdzurman <kamil.dzurman@gmail.com>
2020-02-10 16:57:56 +0100

    Hide buggy keyfile functionality

--- app/frontend/actions.ts
+++ app/frontend/actions.ts
@@ -104,7 +104,7 @@ export default ({
   const setAuthMethod = (state, option) => {
     setState({
       authMethod: option,
-      showExportOption: option === 'mnemonic' || option === 'file'
+      showExportOption: false // TODO: revert - option === 'mnemonic' || option === 'file'
     })
   }
 
@@ -242,7 +242,7 @@ export default ({
       walletLoadingError: undefined,
       showWalletLoadingErrorModal: false,
       authMethod: 'mnemonic',
-      showExportOption: true
+      showExportOption: false //TODO: revert
     })
   }
 
