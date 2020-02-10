98afacb81a4b842eaca39c3565033cdf4ae8d915
xdzurman <kamil.dzurman@gmail.com>
2020-02-10 14:23:15 +0100

    Replace sync error

--- app/frontend/translations.ts
+++ app/frontend/translations.ts
@@ -8,24 +8,34 @@ const {ADALITE_MIN_DONATION_VALUE} = ADALITE_CONFIG
 const translations = {
   SendAddressInvalidAddress: () => 'Invalid address',
   SendAmountIsNan: () => 'Invalid format: Amount has to be a number',
-  SendAmountIsNotPositive: () => 'Invalid format: Amount has to be a positive number',
+  SendAmountIsNotPositive: () =>
+    'Invalid format: Amount has to be a positive number',
   SendAmountInsufficientFunds: ({balance}) =>
-    `Insufficient funds for the transaction. Your balance is ${printAda(balance)} ADA.`,
+    `Insufficient funds for the transaction. Your balance is ${printAda(
+      balance
+    )} ADA.`,
   SendAmountCantSendAnyFunds: () =>
     'Sending funds is not possible since there is not enough balance to pay the transaction fee',
-  SendAmountPrecisionLimit: () => 'Invalid format: Maximum allowed precision is 0.000001',
+  SendAmountPrecisionLimit: () =>
+    'Invalid format: Maximum allowed precision is 0.000001',
   SendAmountIsTooBig: () =>
-    `Invalid format: Amount cannot exceed ${printAda(Number.MAX_SAFE_INTEGER as Lovelace)}`,
-  DonationAmountTooLow: () => `Minimum donation is ${ADALITE_MIN_DONATION_VALUE} ADA`,
+    `Invalid format: Amount cannot exceed ${printAda(
+      Number.MAX_SAFE_INTEGER as Lovelace
+    )}`,
+  DonationAmountTooLow: () =>
+    `Minimum donation is ${ADALITE_MIN_DONATION_VALUE} ADA`,
   DonationInsufficientBalance: () => 'Insufficient balance for the donation.',
 
   InvalidStakepoolIdentifier: () => 'Stakepool id or ticker is invalid.',
   RudundantStakePool: () => 'This stake pool is already chosen.',
-  DelegationAccountBalanceError: () => 'Not enough funds to pay the delegation fee.',
+  DelegationAccountBalanceError: () =>
+    'Not enough funds to pay the delegation fee.',
 
-  InvalidMnemonic: () => 'Invalid mnemonic, check your mnemonic for typos and try again.',
+  InvalidMnemonic: () =>
+    'Invalid mnemonic, check your mnemonic for typos and try again.',
 
-  TransportOpenUserCancelled: ({message}) => `TransportCanceledByUser: ${message}`,
+  TransportOpenUserCancelled: ({message}) =>
+    `TransportCanceledByUser: ${message}`,
   TransportError: ({message}) => `TransportError: ${message}`,
   TransportStatusError: ({message}) => `TransportStatusError: ${message}`,
 
@@ -33,14 +43,16 @@ const translations = {
     'TransactionRejectedByNetwork: Submitting the transaction into Cardano network failed.',
   TransactionRejectedWhileSigning: ({message}) =>
     `Transaction rejected while signing${message ? `:  ${message}` : '.'}`,
-  TransactionCorrupted: () => 'TransactionCorrupted: Transaction assembling failure.',
+  TransactionCorrupted: () =>
+    'TransactionCorrupted: Transaction assembling failure.',
   TransactionNotFoundInBlockchainAfterSubmission: ({txHash}) =>
     `TransactionNotFoundInBlockchainAfterSubmission: 
     Transaction ${txHash ||
       ''} not found in blockchain after being submitted, check it later please.`,
   TxSerializationError: ({message}) => `TxSerializationError: ${message}`,
 
-  TrezorRejected: () => 'TrezorRejected: Operation rejected by the Trezor hardware wallet.',
+  TrezorRejected: () =>
+    'TrezorRejected: Operation rejected by the Trezor hardware wallet.',
   TrezorSignTxError: ({message}) => `TrezorSignTxError: ${message}`,
   TrezorError: ({message}) => `TrezorError: ${message}`,
   LedgerOperationError: ({message}) => `LedgerOperationError: ${message}`,
@@ -50,7 +62,8 @@ const translations = {
   NetworkError: ({message}) =>
     `NetworkError: connection failed. Please check your network connection. ${message}`,
 
-  NodeOutOfSync: () => 'Servis is temporary unavaible, please try again in 1-2 minutes.',
+  NodeOutOfSync: () =>
+    'Service is temporarily unavailable, please try again in 1-2 minutes.'
 }
 
 function getTranslation(code, params = {}) {
