b841ba27997b7a7a124eec292743f3761f04c619
xdzurman <kamil.dzurman@gmail.com>
2020-02-24 12:19:11 +0100

    Hotfix transaction count tracking

--- server/middlewares/statsGoogleAnalytics.js
+++ server/middlewares/statsGoogleAnalytics.js
@@ -85,22 +85,22 @@ const trackTxSubmissions = mung.jsonAsync(async (body, req, res) => {
       }
 
       if (txSubmissionSuccess === 'successful') {
-        const {txBody} = req.body
+        // const {txBody} = req.body
 
-        const txSentAmount = parseTxBodyOutAmount(txBody)
+        // const txSentAmount = parseTxBodyOutAmount(txBody)
         await trackEvent({
           ...baseEventData,
           action: `${txSubmissionType}:sentOut`,
           label: 'successful payment',
-          value: Math.floor(txSentAmount / 1000000),
+          // value: Math.floor(txSentAmount / 1000000),
         })
 
-        const txTotalAmount = parseTxBodyTotalAmount(txBody)
+        // const txTotalAmount = parseTxBodyTotalAmount(txBody)
         await trackEvent({
           ...baseEventData,
           action: `${txSubmissionType}:sentTotal`,
           label: 'total amount',
-          value: Math.floor(txTotalAmount / 1000000),
+          // value: Math.floor(txTotalAmount / 1000000),
         })
       } else {
         await trackEvent({
