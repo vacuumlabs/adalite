411383ac8ebbcf99f4a1bae9e3290e0090644a7c
Rafael Korbas <rafael.korbas@gmail.com>
2020-06-09 11:05:22 +0200

    Add server error handler
    (cherry picked from commit 1a51af68867b4798b699bd23cfb0ec88b0d92e56)

---
+++ server/middlewares/errorHandler.js
@@ -0,0 +1,11 @@
+const IpDeniedError = require('express-ipfilter').IpDeniedError
+
+module.exports = (err, req, res, next) => {
+  if (err instanceof IpDeniedError) {
+    res.status(401).send('Forbidden')
+  } else {
+    // eslint-disable-next-line no-console
+    console.error(err.stack)
+    res.status(500).send('Internal Server Error')
+  }
+}
