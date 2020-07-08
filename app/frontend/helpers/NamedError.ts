c636bec8b9b6411e72d97e81a98e253bfdf9e384
xdzurman <kamil.dzurman@gmail.com>
2020-07-08 10:11:05 +0200

    Replace errorStack string by causedBy error type

--- app/frontend/helpers/NamedError.ts
+++ app/frontend/helpers/NamedError.ts
@@ -1,13 +1,15 @@
 type OptionalParams = {
   message?: string
-  errorStack?: string
+  causedBy?: Error
 }
 
 function NamedError(name: string, optionalParams: OptionalParams = {}) {
   const e = new Error(optionalParams.message || name)
   e.name = name
   e.message = optionalParams.message || ''
-  e.stack = optionalParams.errorStack || e.stack
+  e.stack = optionalParams.causedBy
+    ? `\nError caused by:\n${optionalParams.causedBy.stack}`
+    : e.stack
   return e
 }
 
