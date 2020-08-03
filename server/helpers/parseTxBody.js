e6e3279c609a162f739e052265d2c6f3735f9d6b
xdzurman <kamil.dzurman@gmail.com>
2020-08-03 14:01:24 +0200

    Fix amount tracking and google analytics

--- server/helpers/parseTxBody.js
+++ server/helpers/parseTxBody.js
@@ -7,12 +7,15 @@ const parseTxBodyOutAmount = (txBody) => {
    * is always the amount intended to be sent out
    * format - [txAux][outputs][0-th output][amount]
    */
-  return cbor.decode(txBody)[0][1][0][1]
+  return cbor.decode(txBody)[0].get(1)[0][1]
 }
 
 const parseTxBodyTotalAmount = (txBody) => {
   // reduce along decodedTxBody[0][1][i-th output][1] to sum all outputs
-  return cbor.decode(txBody)[0][1].reduce((acc, curr) => acc + curr[1], 0)
+  return cbor
+    .decode(txBody)[0]
+    .get(1)
+    .reduce((acc, curr) => acc + curr[1], 0)
 }
 
 module.exports = {
