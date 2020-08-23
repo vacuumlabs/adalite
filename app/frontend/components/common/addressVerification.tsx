4957146c03c3f7ff30f1e90b630bce0bd4004b0c
Peter Benc <peter.benc1@gmail.com>
2020-08-23 16:17:29 +0200

    refactor and add address verification on withdrawals

---
+++ app/frontend/components/common/addressVerification.tsx
@@ -0,0 +1,52 @@
+import {h} from 'preact'
+import actions from '../../actions'
+import {connect} from '../../helpers/connect'
+
+const AddressVerification = ({
+  showVerification,
+  verificationError,
+  verifyAddress,
+  address,
+  waitingForHwWallet,
+  hwWalletName,
+}) =>
+  showVerification &&
+  (verificationError ? (
+    <div className="detail-error" style="float: right;">
+      <div>
+        Verification failed.{' '}
+        <a
+          href="#"
+          className="detail-verify"
+          onClick={(e) => {
+            e.preventDefault()
+            verifyAddress(address)
+          }}
+        >
+          Try again
+        </a>
+      </div>
+    </div>
+  ) : (
+    <a
+      href="#"
+      className="detail-verify"
+      style="float: right;"
+      onClick={(e) => {
+        e.preventDefault()
+        !waitingForHwWallet && verifyAddress(address)
+      }}
+    >
+      {waitingForHwWallet ? 'Verifying address..' : `Verify on ${hwWalletName}`}
+    </a>
+  ))
+
+export default connect(
+  (state) => ({
+    waitingForHwWallet: state.waitingForHwWallet,
+    showVerification: state.shouldShowAddressVerification,
+    verificationError: state.addressVerificationError,
+    hwWalletName: state.hwWalletName,
+  }),
+  actions
+)(AddressVerification)
