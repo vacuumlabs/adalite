dc51a4e01dfc42fe0c34520c365f012162aec690
David Tran Duc <david.tran.duc@vacuumlabs.com>
2020-08-23 22:12:17 +0200

    Add functionality for downloading transactions as CSV file

--- app/frontend/components/pages/txHistory/transactionHistory.tsx
+++ app/frontend/components/pages/txHistory/transactionHistory.tsx
@@ -73,11 +73,79 @@ interface Props {
   transactionHistory: any
 }
 
+const ExportCSV = ({transactionHistory}) => {
+  const range = (n) => [...Array(n).keys()]
+
+  const columnsPerIO = 2 // address, ada amount
+  const delimiter = ','
+  const rowsDelimiter = '\n'
+
+  // Headers
+  const maxInputsCount = Math.max(...transactionHistory.map((t) => t.ctbInputs.length))
+  const maxOutputsCount = Math.max(...transactionHistory.map((t) => t.ctbOutputs.length))
+  const maxInputsColumnCount = maxInputsCount * columnsPerIO
+  const maxOutputsColumnCount = maxOutputsCount * columnsPerIO
+  const inputHeaders = range(maxInputsCount)
+    .map((i) => `Input ${i + 1} address${delimiter}Input ${i + 1} amount`)
+    .join(delimiter)
+  const outputHeaders = range(maxOutputsCount)
+    .map((i) => `Output ${i + 1} address${delimiter}Output ${i + 1} amount`)
+    .join(delimiter)
+
+  const headers =
+    `Transaction ID${delimiter}` +
+    `Received date${delimiter}` +
+    `${inputHeaders}${delimiter}` +
+    `${outputHeaders}${delimiter}` +
+    `Input sum${delimiter}` +
+    `Output sum${delimiter}` +
+    `Fee${delimiter}` +
+    'Effect'
+
+  // Rows
+  const translateIOs = (ctbIOs, maxColumnCount) =>
+    ctbIOs
+      .map(
+        (ctbIO) => `${ctbIO[0]}${delimiter}${printAda(ctbIO[1].getCoin as Lovelace)}${delimiter}`
+      )
+      .join('') + delimiter.repeat(maxColumnCount - ctbIOs.length * columnsPerIO)
+
+  const rows = transactionHistory.map(
+    (transaction) =>
+      `${transaction.ctbId}${delimiter}` +
+      `${new Date(transaction.ctbTimeIssued * 1000).toISOString()}${delimiter}` +
+      `${translateIOs(transaction.ctbInputs, maxInputsColumnCount)}` +
+      `${translateIOs(transaction.ctbOutputs, maxOutputsColumnCount)}` +
+      `${printAda(transaction.ctbInputSum.getCoin as Lovelace)}${delimiter}` +
+      `${printAda(transaction.ctbOutputSum.getCoin as Lovelace)}${delimiter}` +
+      `${printAda(transaction.fee as Lovelace)}${delimiter}` +
+      `${printAda(transaction.effect as Lovelace)}`
+  )
+
+  // Create file
+  const fileContents = `${headers}${rowsDelimiter}${rows.join(rowsDelimiter)}`
+  const filename = 'transactions.csv'
+  const filetype = 'text/plain'
+
+  const dataURI = `data:${filetype};base64,${btoa(fileContents)}`
+
+  return (
+    <a href={dataURI} download={filename} className="download-transactions-text">
+      Export to CSV
+    </a>
+  )
+}
+
 class TransactionHistory extends Component<Props> {
   render({transactionHistory}) {
     return (
       <div className="transactions card">
-        <h2 className="card-title">Transaction History</h2>
+        <div className="transactions-header">
+          <h2 className="card-title">Transaction History</h2>
+          <div className="download-transaction">
+            <ExportCSV transactionHistory={transactionHistory} />
+          </div>
+        </div>
         {transactionHistory.length === 0 ? (
           <div className="transactions-empty">No transactions found</div>
         ) : (
