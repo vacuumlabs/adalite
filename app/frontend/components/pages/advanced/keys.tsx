4097e6f10412ec63e525447154564c72eb355482
David Tran Duc <david.tran.duc@vacuumlabs.com>
2020-10-28 18:01:20 +0100

    Add labels and account public key to the advanced tab

--- app/frontend/components/pages/advanced/keys.tsx
+++ app/frontend/components/pages/advanced/keys.tsx
@@ -2,16 +2,20 @@ import {h} from 'preact'
 import {connect} from '../../../libs/unistore/preact'
 import actions from '../../../actions'
 
-const Keys = ({byronXpub, shelleyXpub}) => {
+const Keys = ({byronXpub, shelleyXpub, accountPubkeyHex}) => {
   return (
     <div className="card">
       <h2 className="card-title small-margin">Keys</h2>
-      <div className="current-delegation-wrapper">
-        <b>{byronXpub.path.join("'/")}'</b>
-        <div className="current-delegation-id">{byronXpub.xpub}</div>
-        <b>{shelleyXpub.path.join("'/")}'</b>
-        <div className="current-delegation-id">{shelleyXpub.xpub}</div>
+      <div className="advanced-label">Byron extended public key ({byronXpub.path.join("'/")}')</div>
+      <div className="advanced-value">{byronXpub.xpub}</div>
+      <div className="advanced-label">
+        Shelley extended public key ({shelleyXpub.path.join("'/")}')
       </div>
+      <div className="advanced-value">{shelleyXpub.xpub}</div>
+      <div className="advanced-label">Reward address</div>
+      <div className="advanced-value">{accountPubkeyHex}</div>
+      <div className="advanced-label">Stake key</div>
+      <div className="advanced-value">{accountPubkeyHex.slice(2)}</div>
     </div>
   )
 }
@@ -20,6 +24,7 @@ export default connect(
   (state) => ({
     shelleyXpub: state.shelleyAccountInfo.shelleyXpub,
     byronXpub: state.shelleyAccountInfo.byronXpub,
+    accountPubkeyHex: state.shelleyAccountInfo.accountPubkeyHex,
   }),
   actions
 )(Keys)
