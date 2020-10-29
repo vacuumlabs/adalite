4a3b338b3d6e120de50cb66fabc04be3489dc680
xdzurman <kamil.dzurman@gmail.com>
2020-10-29 10:38:37 +0100

    Reset poolRegTxError with other pool reg variables

--- app/frontend/actions.ts
+++ app/frontend/actions.ts
@@ -1280,6 +1280,7 @@ export default ({setState, getState}: {setState: SetStateFn; getState: GetStateF
         signature: null,
         plan: null,
       },
+      poolRegTxError: undefined,
     })
   }
 
