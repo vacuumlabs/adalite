7b92ff4f344cf09046200c09bcd5cd0ce6747a99
xdzurman <kamil.dzurman@gmail.com>
2021-02-04 09:59:32 +0100

    Update staking page pools

--- app/frontend/components/pages/staking/stakingPage.tsx
+++ app/frontend/components/pages/staking/stakingPage.tsx
@@ -5,13 +5,18 @@ import submitEmailRaw from '../../../helpers/submitEmailRaw'
 import NamedError from '../../../helpers/NamedError'
 import debugLog from '../../../helpers/debugLog'
 
-const newPools = new Set(['ADLT4', 'ADLT5', 'ADLT6'])
+const newPools = new Set(['ADLT1', 'ADLT0'])
 const ADLTPools = [
+  ['ADLT9', '481d305a21bbbaa85177413676ba350b1e2d7b7456b8008e72b96386'],
+  ['ADLT8', '03fbee96b84daa1bf91c44b51d2e1cbdda53fd504c98aeb6fd4a55b6'],
+  ['ADLT7', 'd5e0f919debe82e4447cf0f0089188ae212f7efbc065a3d819daa17a'],
   ['ADLT6', 'cf69a3eca039d537acd46d5864a54dd8953f0c14be957350905834aa'],
   ['ADLT5', '936f24e391afc0738c816ae1f1388957b977de3d0e065dc9ba38af8d'],
   ['ADLT4', 'd785ff6a030ae9d521770c00f264a2aa423e928c85fc620b13d46eda'],
   ['ADLT3', '92229dcf782ce8a82050fdeecb9334cc4d906c6eb66cdbdcea86fb5f'],
   ['ADLT2', 'ce19882fd62e79faa113fcaef93950a4f0a5913b20a0689911b6f62d'],
+  ['ADLT1', 'c779f67a0f3a1f2985626e345013015f71cf245dbcc8ac8457f42e56'],
+  ['ADLT0', 'a0fc643d831b144e1dd289f5acf4274aedc331b6a0e4257ef5376520'],
   ['ADLT', '04c60c78417132a195cbb74975346462410f72612952a7c4ade7e438'],
 ]
 
