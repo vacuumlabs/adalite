c814e91abffc14713c727748673e1f41f4fffd4b
Richard <richardizip@gmail.com>
2021-03-24 09:22:10 +0100

    Avoid redirect for review apps

--- server/middlewares/redirectToBaseUrl.js
+++ server/middlewares/redirectToBaseUrl.js
@@ -1,5 +1,9 @@
 module.exports = (req, res, next) => {
-  if (`${req.protocol}://${req.get('host')}` !== process.env.ADALITE_SERVER_URL) {
+  if (process.env.HEROKU_IS_REVIEW_APP === 'true') {
+    // No redirect for review apps
+    // We can not set `ADALITE_SERVER_URL` for them as their URLs are unique and generated
+    next()
+  } else if (`${req.protocol}://${req.get('host')}` !== process.env.ADALITE_SERVER_URL) {
     res.redirect(301, `${process.env.ADALITE_SERVER_URL}${req.originalUrl}`)
   } else {
     next()
