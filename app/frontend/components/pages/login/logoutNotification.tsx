78a64b78e35fc988a9abc8783ea71f64cd14f13a
xdzurman <kamil.dzurman@gmail.com>
2021-04-09 11:28:23 +0200

    Swap connect for hooks: logoutNotification

--- app/frontend/components/pages/login/logoutNotification.tsx
+++ app/frontend/components/pages/login/logoutNotification.tsx
@@ -1,43 +1,38 @@
 import {h} from 'preact'
-import {connect} from '../../../helpers/connect'
+import {useActions} from '../../../helpers/connect'
 import actions from '../../../actions'
 import Modal from '../../common/modal'
+import {useEffect, useRef} from 'preact/hooks'
 
-class LogoutNotification {
-  understandDemoBtn: any
+const LogoutNotification = (): h.JSX.Element => {
+  const {setLogoutNotificationOpen} = useActions(actions)
+  const understandBtn = useRef<HTMLButtonElement>(null)
 
-  componentDidMount() {
-    this.understandDemoBtn.focus()
-  }
+  useEffect(() => {
+    understandBtn.current.focus()
+  }, [])
 
-  render({setLogoutNotificationOpen}) {
-    return (
-      <Modal
-        onRequestClose={() => setLogoutNotificationOpen(false)}
-        title="You’ve been logged out"
-        bodyClass="centered"
-      >
-        <p className="modal-paragraph">We’ve logged you out after 15 minutes of inactivity.</p>
-        <div className="modal-footer">
-          <button
-            className="button primary"
-            onClick={() => setLogoutNotificationOpen(false)}
-            onKeyDown={(e) => {
-              ;['Enter', 'Escape'].includes(e.key) && (e.target as HTMLButtonElement).click()
-            }}
-            ref={(element) => {
-              this.understandDemoBtn = element
-            }}
-          >
-            Continue
-          </button>
-        </div>
-      </Modal>
-    )
-  }
+  return (
+    <Modal
+      onRequestClose={() => setLogoutNotificationOpen(false)}
+      title="You’ve been logged out"
+      bodyClass="centered"
+    >
+      <p className="modal-paragraph">We’ve logged you out after 15 minutes of inactivity.</p>
+      <div className="modal-footer">
+        <button
+          className="button primary"
+          onClick={() => setLogoutNotificationOpen(false)}
+          onKeyDown={(e) => {
+            ;['Enter', 'Escape'].includes(e.key) && (e.target as HTMLButtonElement).click()
+          }}
+          ref={understandBtn}
+        >
+          Continue
+        </button>
+      </div>
+    </Modal>
+  )
 }
 
-export default connect(
-  null,
-  actions
-)(LogoutNotification)
+export default LogoutNotification
