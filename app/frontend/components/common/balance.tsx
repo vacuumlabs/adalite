6af281fe2e3803fd24c389b29f5e40df260f669d
Richard Ižip <richardizip@gmail.com>
2021-04-30 15:44:10 +0200

    Setup CSS-modules (#965)
    * Example usage with "Balance" component
    * Introduce sass and sass-variables
    * Remove "connect" from balance component

--- app/frontend/components/common/balance.tsx
+++ app/frontend/components/common/balance.tsx
@@ -1,55 +1,54 @@
 import {h} from 'preact'
 import printAda from '../../helpers/printAda'
 import Conversions from './conversions'
-import {State, getActiveAccountInfo} from '../../state'
+import {getActiveAccountInfo} from '../../state'
 import {AdaIcon} from './svg'
 import actions from '../../actions'
-import {connect} from '../../libs/unistore/preact'
+import {useSelector, useActions} from '../../helpers/connect'
 import {Lovelace} from '../../types'
+import styles from './balance.module.scss'
 
-interface Props {
-  balance: Lovelace
-  reloadWalletInfo: (state: any) => void
-  conversionRates: any
+const Balance = () => {
+  const {reloadWalletInfo} = useActions(actions)
+  const {conversionRates, balance} = useSelector((state) => ({
+    conversionRates: state.conversionRates && state.conversionRates.data,
+    balance: getActiveAccountInfo(state).balance as Lovelace,
+  }))
+  return (
+    <div className="balance card">
+      <h2 className={`card-title ${styles.balanceTitle}`}>Available balance</h2>
+      <div className={styles.balanceRow}>
+        <div className="balance-amount" data-cy="SendBalanceAmount">
+          {isNaN(Number(balance)) ? balance : `${printAda(balance)}`}
+          <AdaIcon />
+        </div>
+        <button
+          className={`button secondary balance ${styles.refreshButton}`}
+          onClick={reloadWalletInfo}
+        >
+          Refresh
+        </button>
+      </div>
+      {conversionRates && <Conversions balance={balance} conversionRates={conversionRates} />}
+      {/* <div className="buy-ada-partner-wrapper">
+        <a
+          className="button primary outline buy-ada-partner-link"
+          href="https://exchange.adalite.io/"
+          target="_blank"
+        >
+          Buy/Sell ADA
+        </a>
+        <span className="buy-ada-partner-logo">
+          Powered by
+          <img
+            className="buy-ada-partner-logo-img"
+            src="assets/coinswitch-logo.svg"
+            alt="CoinSwitch logo"
+          />
+        </span>
+      </div> */}
+    </div>
+  )
 }
 
-const Balance = ({balance, reloadWalletInfo, conversionRates}: Props) => (
-  <div className="balance card">
-    <h2 className="card-title balance-title">Available balance</h2>
-    <div className="balance-row">
-      <div className="balance-amount" data-cy="SendBalanceAmount">
-        {isNaN(Number(balance)) ? balance : `${printAda(balance)}`}
-        <AdaIcon />
-      </div>
-      <button className="button secondary balance refresh" onClick={reloadWalletInfo}>
-        Refresh
-      </button>
-    </div>
-    {conversionRates && <Conversions balance={balance} conversionRates={conversionRates} />}
-    {/* <div className="buy-ada-partner-wrapper">
-      <a
-        className="button primary outline buy-ada-partner-link"
-        href="https://exchange.adalite.io/"
-        target="_blank"
-      >
-        Buy/Sell ADA
-      </a>
-      <span className="buy-ada-partner-logo">
-        Powered by
-        <img
-          className="buy-ada-partner-logo-img"
-          src="assets/coinswitch-logo.svg"
-          alt="CoinSwitch logo"
-        />
-      </span>
-    </div> */}
-  </div>
-)
-
-export default connect(
-  (state: State) => ({
-    conversionRates: state.conversionRates && state.conversionRates.data,
-    balance: getActiveAccountInfo(state).balance,
-  }),
-  actions
-)(Balance)
+export default Balance
