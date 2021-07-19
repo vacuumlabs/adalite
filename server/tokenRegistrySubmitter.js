80881e32fe707db37ff4e60c922c00a7e09d9878
David Tran Duc <david.tran.duc@vacuumlabs.com>
2021-07-19 21:01:44 +0200

    [MOCK] Delete before merging, add metadata to firstasset so we can see how metadata looks on UI

--- server/tokenRegistrySubmitter.js
+++ server/tokenRegistrySubmitter.js
@@ -1,6 +1,7 @@
 require('isomorphic-fetch')
 const Sentry = require('@sentry/node')
 const {backendConfig} = require('./helpers/loadConfig')
+const mockData = require('./_tokenRegistryMock')
 
 module.exports = function(app, env) {
   app.post('/api/tokenRegistry/getTokensMetadata', async (req, res) => {
@@ -22,6 +23,13 @@ module.exports = function(app, env) {
           })
           .filter((e) => e !== null)
       )
+
+      // MOCK, delete relevant commit before merging
+      if (
+        req.body.subjects.includes(
+          'ca37dd6b151b6a1d023ecbd22d7e881d814b0c58a3a3148b42b865a066697273746173736574'
+        )
+      ) {tokensMetadata.push(mockData)}
 
       return res.json({
         Right: tokensMetadata,
