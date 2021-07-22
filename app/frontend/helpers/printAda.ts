fe2fa1b2dd901cea3f5ae430ef767c501eac9784
David Tran Duc <david.tran.duc@vacuumlabs.com>
2021-07-22 10:33:53 +0200

    Refactor decimals to precision in printAda helper function

--- app/frontend/helpers/printAda.ts
+++ app/frontend/helpers/printAda.ts
@@ -1,12 +1,12 @@
 import {Lovelace} from '../types'
-export default (coins: Lovelace, decimals = 6): string => {
+export default (coins: Lovelace, precision = 6): string => {
   const adaAmount = coins * 0.000001
 
-  if (decimals === 0) {
+  if (precision === 0) {
     return Math.floor(adaAmount).toString()
-  } else if (decimals > 0 && decimals <= 6) {
-    return adaAmount.toFixed(7).slice(0, -7 + decimals)
+  } else if (precision > 0 && precision <= 6) {
+    return adaAmount.toFixed(7).slice(0, -7 + precision)
   } else {
-    return adaAmount.toFixed(decimals)
+    return adaAmount.toFixed(precision)
   }
 }
