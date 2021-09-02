68becc7b7898f2b4be9ab1a6306092520191dc70
David Tran Duc <david.tran.duc@vacuumlabs.com>
2021-09-02 19:56:11 +0200

    Use types from trezor-connect instead of redeclared types
    Update logic to work with witnesses returned from Trezor instead of whole signed transaction

--- app/frontend/helpers/removeNullFiels.ts
+++ app/frontend/helpers/removeNullFiels.ts
@@ -1,4 +1,4 @@
-export const removeNullFields = (obj: any) =>
+export const removeNullFields = (obj: any): any =>
   Object.keys(obj)
     .filter((key) => obj[key] !== null && obj[key] !== undefined)
     .reduce((acc, key) => {
