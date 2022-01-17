eccfd55b33a75f0617387dea9ab439c7542dd021
refi93 <rafael.korbas@gmail.com>
2022-01-17 09:21:28 +0100

    Fix transport choice for devices that support none of Ledger transports

--- app/frontend/wallet/shelley/helpers/transports.ts
+++ app/frontend/wallet/shelley/helpers/transports.ts
@@ -2,6 +2,7 @@ import {isWindows, isOpera} from 'react-device-detect'
 
 import LedgerTransportWebUsb from '@ledgerhq/hw-transport-webusb'
 import LedgerTransportWebHid from '@ledgerhq/hw-transport-webhid'
+import LedgerTransportU2F from '@ledgerhq/hw-transport-u2f'
 import {LedgerTransportType, LedgerTransportChoice} from '../../../types'
 
 const isWebUsbSupported = async (): Promise<boolean> => {
@@ -16,16 +17,23 @@ const isWebHidSupported = async (): Promise<boolean> => {
   return isSupported && !isOpera
 }
 
+const isU2FSupported = async (): Promise<boolean> => {
+  return await LedgerTransportU2F.isSupported()
+}
+
 const getDefaultLedgerTransportType = async (): Promise<LedgerTransportType> => {
   const supportWebHid = await isWebHidSupported()
   const supportWebUsb = await isWebUsbSupported()
+  const supportU2F = await isU2FSupported()
 
   if (supportWebHid) {
     return LedgerTransportChoice.WEB_HID
   } else if (supportWebUsb) {
     return LedgerTransportChoice.WEB_USB
-  } else {
+  } else if (supportU2F) {
     return LedgerTransportChoice.U2F
+  } else {
+    return LedgerTransportChoice.WEB_HID
   }
 }
 
