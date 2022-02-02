05d8316ea182cd38ad9f6cadeb4262aac6f8e1a9
refi93 <rafael.korbas@gmail.com>
2022-02-02 17:44:21 +0100

    Add warning about congestion to News

--- app/frontend/localStorage.ts
+++ app/frontend/localStorage.ts
@@ -1,6 +1,6 @@
 const localStorageVars = {
   WELCOME: 'dontShowDisclaimer',
-  INFO_MODAL: 'dontShowInfoModal_15.12.2021',
+  INFO_MODAL: 'dontShowInfoModal_03.02.2022',
   PREMIUM_BANNER: 'dontShowPremiumBanner',
   STAKING_BANNER: 'dontShowStakingBannerTestnet3',
   BULK_EXPORT: 'dontBulkExportPubKeys',
