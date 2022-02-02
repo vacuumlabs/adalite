512423bf1d9fe981f8c2aa05186cd6a3073a8c4d
refi93 <rafael.korbas@gmail.com>
2022-02-02 17:44:21 +0100

    Add warning about congestion to News

--- app/frontend/localStorage.ts
+++ app/frontend/localStorage.ts
@@ -1,10 +1,14 @@
 const localStorageVars = {
   WELCOME: 'dontShowDisclaimer',
 <<<<<<< HEAD
+<<<<<<< HEAD
   INFO_MODAL: 'dontShowInfoModal_15.12.2021',
 =======
   INFO_MODAL: 'dontShowInfoModal_03.02.2022',
 >>>>>>> dd236285... Update news modal date
+=======
+  INFO_MODAL: 'dontShowInfoModal_02.02.2022',
+>>>>>>> 19c2a222... Add warning about congestion to News
   PREMIUM_BANNER: 'dontShowPremiumBanner',
   STAKING_BANNER: 'dontShowStakingBannerTestnet3',
   BULK_EXPORT: 'dontBulkExportPubKeys',
