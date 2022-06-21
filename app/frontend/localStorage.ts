4730dbbb2d7c56e17a7504662535808c04f1ef4c
AndrejZelonka <andrej.zelonka@gmail.com>
2022-06-21 14:11:37 +0200

    Add NuFi chrome extension announcement

--- app/frontend/localStorage.ts
+++ app/frontend/localStorage.ts
@@ -1,6 +1,6 @@
 const localStorageVars = {
   WELCOME: 'dontShowDisclaimer',
-  INFO_MODAL: 'dontShowInfoModal_03.02.2022',
+  INFO_MODAL: 'dontShowInfoModal_14.07.2022',
   PREMIUM_BANNER: 'dontShowPremiumBanner',
   STAKING_BANNER: 'dontShowStakingBannerTestnet3',
   BULK_EXPORT: 'dontBulkExportPubKeys',
