ab32b13f482e6701113e23a7ae1f3f681183b9fc
Andrej Želonka <andrej.zelonka@gmail.com>
2024-02-28 09:39:13 +0100

    Revert "Add banner for catalyst proposal"
    This reverts commit ea14820304b894e43f6df22a5b151e8f1b0423cc.

--- app/frontend/components/pages/login/loginPage.tsx
+++ app/frontend/components/pages/login/loginPage.tsx
@@ -8,6 +8,8 @@ import HardwareAuth from './hardwareAuth'
 import GenerateMnemonicDialog from './generateMnemonicDialog'
 import LogoutNotification from './logoutNotification'
 import LoginPageSidebar from './loginPageSidebar'
+import StakingBanner from './stakingBanner'
+import ErrorBanner from './errorBanner'
 import Tag from '../../common/tag'
 import WalletLoadingErrorModal from './walletLoadingErrorModal'
 import {getErrorHelpType, getErrorMessage} from '../../../errors'
@@ -212,39 +214,37 @@ const LoginPage = () => {
     walletLoadingError,
     shouldShowGenerateMnemonicDialog,
     shouldShowWalletLoadingErrorModal,
+    shouldShowStakingBanner,
     autoLogin,
+    errorBannerContent,
   } = useSelector((state: State) => ({
     authMethod: state.authMethod,
     shouldShowLogoutNotification: state.shouldShowLogoutNotification,
     walletLoadingError: state.walletLoadingError,
     shouldShowGenerateMnemonicDialog: state.shouldShowGenerateMnemonicDialog,
     shouldShowWalletLoadingErrorModal: state.shouldShowWalletLoadingErrorModal,
+    shouldShowStakingBanner: state.shouldShowStakingBanner,
     autoLogin: state.autoLogin,
+    errorBannerContent: state.errorBannerContent,
   }))
-  const {setAuthMethod, closeWalletLoadingErrorModal} = useActions(actions)
+  const {
+    closeStakingBanner,
+    setAuthMethod,
+    closeWalletLoadingErrorModal,
+    loadErrorBannerContent,
+  } = useActions(actions)
 
   useEffect(() => {
     if (autoLogin && authMethod !== AuthMethodType.MNEMONIC) {
       setAuthMethod(AuthMethodType.MNEMONIC)
     }
+    loadErrorBannerContent()
   }, []) // eslint-disable-line
 
   return (
     <div className="page-wrapper">
-      <div className="banner success">
-        <p>
-          {' '}
-          We need your help! The AdaLite/NuFi team has submitted 2 proposals in Project Catalyst
-          Fund11 and your vote can help us win funding.{' '}
-          <a
-            target="_blank"
-            href="https://nufi-official.medium.com/2024-the-year-the-world-discovered-cardano-thanks-to-nufis-project-catalyst-proposals-14cdeda63f1e"
-          >
-            Learn more
-          </a>
-          .
-        </p>
-      </div>
+      {shouldShowStakingBanner && <StakingBanner onRequestClose={() => closeStakingBanner()} />}
+      {errorBannerContent && <ErrorBanner message={errorBannerContent} />}
       <div className="page-inner">
         <main className="page-main">
           <NufiBannerWrapper />
