1816489ebf060ab537ede023c75b54547277d8fb
Andrej Želonka <andrej.zelonka@gmail.com>
2025-07-15 16:42:21 +0200

    feat: update static nufi banners

--- app/frontend/components/common/nufiBanner.tsx
+++ app/frontend/components/common/nufiBanner.tsx
@@ -1,21 +1,23 @@
 import {h} from 'preact'
 
 const NufiBanner = ({variant = 'static'}: {variant: 'static' | 'gif'}) => {
+  const {src, link} = (() => {
+    if (variant === 'gif') {
+      return {
+        src: 'assets/nufi-animated-banner-light.gif',
+        link: 'https://nu.fi/?utm_campaign=nufi-wallet-gif-banner',
+      }
+    }
+
+    const staticBannerNumber = Math.floor(Math.random() * 3) + 1 // random number between 1 and 3
+    return {
+      src: `assets/nufi-animated-banner-light-${staticBannerNumber}.png`,
+      link: `https://nu.fi/?utm_campaign=nufi-wallet-static-banner-${staticBannerNumber}`,
+    }
+  })()
   return (
-    <a className="banner nufi" href="https://nu.fi" rel="noopener" target="blank">
-      {variant === 'static' ? (
-        <img
-          src="assets/nufi-animated-banner-light.png"
-          alt="NuFi - Crypto Wallet"
-          style={{width: '100%'}}
-        />
-      ) : (
-        <img
-          src="assets/nufi-animated-banner-light.gif"
-          alt="NuFi - Crypto Wallet"
-          style={{width: '100%'}}
-        />
-      )}
+    <a className="banner nufi" href={link} rel="noopener" target="blank">
+      <img src={src} alt="NuFi - Crypto Wallet" style={{width: '100%'}} />
     </a>
   )
 }
