2cda4254d96b76ab39e33d0ba5952407640f916c
refi93 <rafael.korbas@gmail.com>
2025-07-16 11:25:17 +0200

    refactor: polish banner logic using react hooks

--- app/frontend/components/common/nufiBanner.tsx
+++ app/frontend/components/common/nufiBanner.tsx
@@ -1,25 +1,34 @@
 import {h} from 'preact'
+import {useMemo} from 'preact/hooks'
 
 const NufiBanner = ({variant = 'static'}: {variant: 'static' | 'gif'}) => {
-  const {src, link} = (() => {
-    if (variant === 'gif') {
-      return {
-        src: 'assets/nufi-animated-banner-light.gif',
-        link: 'https://nu.fi/?utm_campaign=nufi-wallet-gif-banner',
-      }
-    }
+  const {src, link} = useMemo(() => getRandomizedBannerProps(variant), [variant])
 
-    const staticBannerNumber = Math.floor(Math.random() * 3) + 1 // random number between 1 and 3
-    return {
-      src: `assets/nufi-animated-banner-light-${staticBannerNumber}.png`,
-      link: `https://nu.fi/?utm_campaign=nufi-wallet-static-banner-${staticBannerNumber}`,
-    }
-  })()
   return (
     <a className="banner nufi" href={link} rel="noopener" target="blank">
       <img src={src} alt="NuFi - Crypto Wallet" style={{width: '100%'}} />
     </a>
   )
+}
+
+type BannerProps = {
+  src: string
+  link: string
+}
+
+const getRandomizedBannerProps = (variant: 'static' | 'gif'): BannerProps => {
+  if (variant === 'gif') {
+    return {
+      src: 'assets/nufi-animated-banner-light.gif',
+      link: 'https://nu.fi/?utm_campaign=nufi-wallet-gif-banner',
+    }
+  }
+
+  const staticBannerNumber = Math.floor(Math.random() * 3) + 1 // random number between 1 and 3
+  return {
+    src: `assets/nufi-animated-banner-light-${staticBannerNumber}.png`,
+    link: `https://nu.fi/?utm_campaign=nufi-wallet-static-banner-${staticBannerNumber}`,
+  }
 }
 
 export default NufiBanner
